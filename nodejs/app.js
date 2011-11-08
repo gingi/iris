4995daac0ebe549d5f7a491d47b2b75559f1775c
Andrew Olson <olson@asiago.cshl.edu>
2011-11-08 11:41:17 -0500

    setup manhattanproxy for node.js implementation

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,7 +5,8 @@
 
 var express = require('express')
   , routes = require('./routes')
-
+	, util = require('util')
+	, http = require('http')
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -34,6 +35,44 @@ app.get('/gwas', routes.select_study);
 app.get('/gwas/:study([0-9]+)', function(req,res) {
 	routes.gwas(req.params.study,res);
 });
+app.get('/manhattanproxy/get_chromosomes', function(req,res) {
+	var options = {
+		host: 'brie.cshl.edu',
+		port: 80,
+		path: '/~olson/qdv/web/chr_list.pl?g=at'
+	};
+	http.get(options, function(response) {
+		util.pump(response, res);
+	});
+});
+
+app.get('/manhattanproxy/get_scores/:study/:id', function(req,res) {
+	var options = {
+		host: 'brie.cshl.edu',
+		port: 80,
+		path: '/~olson/qdv/web/run.pl?exe=fbsql&s=chr,min(score),max(score)'
+			+ '&d=GWAS/' + req.params.study
+			+ '&w=id=\''+req.params.id+'\''
+	};
+	http.get(options, function(response) {
+		util.pump(response, res);
+	});
+});
+app.get('/manhattanproxy/get_coordinates/:bins/:study/:experiment/:chromosome', function(req,res) {
+	var options = {
+		host: 'brie.cshl.edu',
+		port: 80,
+		path: '/~olson/qdv/web/run.pl?exe=get2DDist&c1=pos&c2=score'
+			+ '&b='	+ req.params.bins
+			+ '&d=GWAS/' + req.params.study
+			+ '&w=id=\'' + req.params.experiment + '\'+and+chr=' + req.params.chromosome
+	};
+	http.get(options, function(response) {
+		util.pump(response, res);
+	});
+});
+
+
 
 app.listen(3003);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
