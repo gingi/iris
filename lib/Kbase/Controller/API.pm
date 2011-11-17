4c8a308bc71d22b9e0896aee56985977060b7a58
Shiran Pasternak <shiranpasternak@gmail.com>
2011-11-17 18:14:33 -0500

    A few more modifications, now displays ugly big discs/ellipses, with mouseover functionality.

--- lib/Kbase/Controller/API.pm
+++ lib/Kbase/Controller/API.pm
@@ -31,11 +31,25 @@ sub fastbit : Local : ActionClass('REST') {
 
 sub fastbit_GET {
     my ($self, $c) = @_;
-    my $uri  = "http://wildcat:3001/scatter/GWAS/3396/1/100/150";
+
+    my $uri  = 'http://wildcat:3001/scatter/GWAS/3396/1/800/800';
     my $ua   = LWP::UserAgent->new;
     my $data = $ua->get($uri);
     $self->status_ok($c,
         entity => JSON::XS->new->utf8->decode($data->content));
+=pod
+    $self->status_ok(
+        $c,
+        entity => {
+            data => [ [ 657, 7607981, 0, 2.563327, 10710, 6 ] ],
+            max1 => 30429953,
+            max2 => 10.253307,
+            min1 => 657,
+            min2 => 0,
+            total => 52381
+        }
+    );
+=cut
 }
 
 =head1 AUTHOR
--- root/static/javascript/dots.js
+++ root/static/javascript/dots.js
@@ -20,46 +20,103 @@ $(function() {
             color = $("#chart").css("color");
         var plotW = width - leftgutter;
         var plotH = height - bottomgutter;
-        var bp2px = Math.ceil(parseInt(json["max1"])/plotW);
-        var p2px  = Math.ceil(parseInt(json["max2"])/plotH);
+        console.log("plot[]", plotW, plotH);
+        var bp2px = Math.ceil(parseFloat(json["max1"])/plotW);
+        var p2px  = parseFloat(json["max2"])/plotH;
+        console.log("2px", bp2px, p2px);
         var xticks = 10; // TODO: configure
+        var yticks = 10;
 
-        for (var i = 0; i <= 10; i++) {
-            axisy.push((1-i/10).toFixed(1));
-        }
+        // for (var i = yticks; i >= 0; i--)
+        //     axisy.push((i * p2px * plotH / yticks).toFixed(1));
+        for (var i = 0; i <= yticks; i++)
+            axisy.push((i * plotH / yticks).toFixed(0));
         for (var i = 0; i <= xticks; i++)
             axisx.push((i * bp2px * plotW / xticks).toFixed(0));
-        var X = (width - leftgutter) / axisx.length,
-            Y = (height - bottomgutter) / axisy.length;
-        max = Math.round(X / 2) - 1;
+        var X = (width - leftgutter) / (axisx.length - 1),
+            Y = (height - bottomgutter) / (axisy.length - 1);
+        max = 100; //Math.round(X / 2) - 1;
+        r.rect(leftgutter, 0, plotW, plotH).attr({
+            stroke: "#fff"
+        });
         var radius = function radius(value) {
-            return Math.min(Math.round(Math.sqrt(Math.PI / value)), max);
+            return Math.min(Math.round(Math.sqrt(value)), max);
         };
-        // r.rect(0, 0, width, height, 5).attr({fill: "#000", stroke: "none"});
+        var toX = function(lo, hi) {
+            if (hi == null) hi = lo;
+            return leftgutter + Math.round((lo + hi)/(2*bp2px));
+        };
+        var toY = function(lo, hi) {
+            if (hi == null) hi = lo;
+            return height - bottomgutter - Math.round((lo + hi)/(2*p2px));
+        };
+        var toH = function(h) { return Math.round(h/bp2px); }
+        var toW = function(w) { return Math.round(w/p2px); }
         for (var i = 0, ii = axisx.length; i < ii; i++) {
-            r.text(leftgutter + X * (i + .5), height - 8, axisx[i]).attr(txt);
+            r.text(leftgutter + X * (i + 0), height - 8, axisx[i]).attr(txt);
         }
         for (var i = 0, ii = axisy.length; i < ii; i++) {
-            r.text(10, Y * (i + .5), axisy[i]).attr(txt);
+            r.text(10, Y * (i + 0), axisy[i]).attr(txt);
         }
         var o = 0;
         var data = json['data'];
         jQuery.each(data, function(i, rect) {
-            if (i > 100) { return; }
+            // if (rect[4] > 5 || i > 10000) { return; }
             var center = [
-                leftgutter + Math.round((rect[1] - rect[0])/bp2px),
-                height - bottomgutter - Math.round((rect[3] - rect[2])/p2px)
+                toX(rect[0], rect[1]),
+                toY(rect[2], rect[3])
             ];
-            console.log(rect, center);
-            var dot = r.circle(center[0], center[1], radius(rect[4])).attr({
-                fill: "hsb(1, 1, 1)",
+            var R = radius(rect[4]);
+            var color = "rgba(255, 0, 0, 0.7)";// + Math.min(rect[4]/100 + 0.5, 1) + ")";
+            var dt = r.circle(center[0], center[1], R).attr({
+                stroke: "none",
+                fill: color
+            });
+            // var dot = r.rect(toX(rect[0]),         toY(rect[3]),
+            //                  toH(rect[1]-rect[0]), toW(rect[3]-rect[2])
+            //                  ).attr({
+            //                      fill: "rgba(0, 128, 0, " + Math.min(rect[4]/100 + 0.5, 1) + ")",
+            //                      stroke: "none"
+            //                  });
+            var dot = r.circle(center[0], center[1], R)
+            .attr({
+                fill: color,
                 stroke: "none"
             });
+            if (R < 6) {
+                var bg = r.circle(center[0], center[1], 6).attr({
+                    stroke: "none",
+                    fill: "#666",
+                    opacity: .7
+                }).hide();
+            }
+            var lbl = r.text(center[0], center[1], rect[4] + " points")
+            .attr({
+                "font": '10px Fontin-Sans, Arial',
+                stroke: "none",
+                fill: "#fff"
+            }).hide();
+            dot[0].onmouseover = function() {
+                if (bg) {
+                    bg.show();
+                } else {
+                    var clr = Raphael.rgb2hsb(color);
+                    clr.b = .5;
+                    dt.attr("fill", Raphael.hsb2rgb(clr).hex);
+                }
+                lbl.show();
+            };
+            dot[0].onmouseout = function() {
+                if (bg) {
+                    bg.hide();
+                } else {
+                    dt.attr("fill", color);
+                }
+                lbl.hide();
+            };
         });
         for (var i = 0, ii = axisy.length; i < ii; i++) {
-            // console.log("i:" + i + " ii:" + ii + " max:" + max);
             for (var j = 0, jj = axisx.length; j < jj; j++) {
-                // console.log(Math.min(Math.round(Math.sqrt(Math.PI / Math.PI) * 4)));
                 var R = data[o] && Math.min(Math.round(Math.sqrt(data[o] / Math.PI) * 4), max);
                 if (R) {
                     (function(dx, dy, R, value) {
