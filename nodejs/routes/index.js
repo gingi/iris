70dedceb36b00bf70f8e4c72a8278038fb39daac
chia <chia@cshl.edu>
2011-11-18 18:10:05 -0500

    Adding routes to test fastbit-json-jquery

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -5,3 +5,8 @@
 exports.index = function(req, res){
   res.render('index', { title: 'lets try jquery' })
 };
+
+exports.jquery_fastbit = function(req, res){
+	console.log("going here")
+	res.render('jquery_fastbit',  { layout:false, title: 'Testing Bed for fastbit and jQuery' })
+};
