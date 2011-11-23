2145c2c09ceaea9a415d7ab684c0ef70b8d5a9f6
Andrew Olson <olson@cshl.edu>
2011-11-23 10:04:55 -0500

    (split)GWAS plot all points

--- public/javascripts/canvas_scatter.js
+++ public/javascripts/canvas_scatter.js
@@ -66,3 +66,59 @@ function do_scatter(ctx,study,chr,offset,xsize,ysize,chr_length) {
 			}
 		});
 }
+
+function manhattan_plot_dots(canvasid,study) {
+	// fetch the list of chromosomes and their lengths
+	$.getJSON("/chromosomes/at",
+		function(json1) {
+			for(var i=0;i<json1.length;i++) {
+				chr_lengths[i] = json1[i][1];
+				total_len += json1[i][1];
+			}
+			// fetch the max score for this study
+			$.getJSON("/maxscore/GWAS/"+study,
+			function(json2) {
+				global_max = Math.ceil(json2[0][0]);
+				draw_manhattan_dots(canvasid,study);
+			}
+		);
+	});
+}
+
+function draw_manhattan_dots(canvasid,study) {
+  // get the canvas
+	var canvas = document.getElementById(canvasid);
+	var ctx = canvas.getContext('2d');
+	ctx.canvas.width = window.innerWidth-20;
+	ctx.canvas.height = window.innerHeight - 20;
+	ctx.strokeStyle="black";
+	ctx.strokeRect(0,0,ctx.canvas.width,ctx.canvas.height);
+	var offset=0;
+	var ysize = ctx.canvas.height;
+	for(var i=0;i<chr_lengths.length;i++) {
+		var xsize = ctx.canvas.width*(chr_lengths[i]/total_len);
+		do_scatter_dots(ctx,study,i+1,offset,xsize,ysize,chr_lengths[i]);
+		offset += xsize;
+	}
+}
+
+
+function do_scatter_dots(ctx,study,chr,offset,xsize,ysize,chr_length) {
+	$.getJSON("/scatter/GWAS/nobinning/"+study+"/"+chr,
+		function(json) {
+			var xrange = chr_length;
+			var yrange = global_max;
+			var xfactor = xsize/xrange;
+			var yfactor = ysize/yrange;
+			if (chr % 2 === 0) {
+				ctx.fillStyle="darkorange";
+			} else {
+				ctx.fillStyle="black";
+			}
+			for(var i=0; i<json.length; i++) {
+				var x = xfactor*json[i][0] + offset;
+				var y = ysize - 2 - yfactor*json[i][1];
+				ctx.fillRect(Math.floor(x),Math.floor(y),2,2);
+			}
+		});
+}
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,14 @@ exports.gwas = function(study, res) {
 		+ '<canvas id="scatter" width="1100" height="500"></canvas>');
 };
 
+exports.allpoints = function(study,res) {
+	res.writeHead(200,{"Content-type":"text/html"});
+	res.end('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>'
+		+ '<script type="text/javascript" src="/javascripts/canvas_scatter.js"></script>'
+		+ '<script type="text/javascript">$(document).ready(manhattan_plot_dots("scatter",'+study+'))</script>'
+		+ '<canvas id="scatter" width="1100" height="500"></canvas>');
+};
+
 exports.jquery_fastbit = function(req, res){
 	console.log("going here")
 	res.render('jquery_fastbit',  { layout:false, title: 'Routing fastbit json jQuery' })
