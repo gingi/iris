fc8e76a2051343d7411ae8071044c11c42100707
Andrew Olson <olson@cshl.edu>
2011-11-23 02:16:42 -0500

    (split)manhattan on canvas

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'lets try jquery' })
+  res.render('index', { title: 'just paint on the canvas' })
+};
+
+exports.gwas = function(study, res) {
+	res.writeHead(200,{"Content-type":"text/html"});
+	res.end('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>'
+		+ '<script type="text/javascript" src="/javascripts/canvas_scatter.js"></script>'
+		+ '<script type="text/javascript">$(document).ready(manhattan_plot("scatter",'+study+'))</script>'
+		+ '<canvas id="scatter" width="1100" height="500"></canvas>');
 };
