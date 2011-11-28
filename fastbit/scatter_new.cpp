3153d613eeec8da85fa2b80564161ece63305aa5
Andrew Olson <olson@cshl.edu>
2011-11-28 18:11:59 -0500

    some fixes

--- fastbit/scatter_new.cpp
+++ fastbit/scatter_new.cpp
@@ -13,6 +13,7 @@ const char* fmt_float = "%f";
 const char* f1;
 const char* f2;
 ibis::table* tbl=0;
+int debug=0;
 
 
 // printout the usage string
@@ -30,6 +31,7 @@ static void usage(const char* name) {
 			<< "[-x1 max for c1]\n"
 			<< "[-x2 max for c2]\n"
 	      << "[-w where-clause]\n"
+			<< "[-v verboseness]\n"
 	      << std::endl;
 } // usage
 
@@ -65,6 +67,12 @@ static void parse_args(int argc, char** argv,
 					if (i+1 < argc) {
 						++ i;
 						*minbins = atoi(argv[i]);
+					}
+					break;
+				case 'v':
+					if (i+1 < argc) {
+						++ i;
+						debug = 1;
 					}
 					break;
 	    		case 'b':
@@ -244,79 +252,90 @@ static void fake2DDist(const char *col1, double min1, double max1, const char *c
 		}
 		printf("[");
 		printf(f1,c1);printf(",");
-		printf(f1,c1);printf(",");
-		printf(f2,c2);printf(",");
-		printf(f2,c2);
-		printf(",1,0]\n");
+		printf(f2,c2);printf("]");
 	}
 	delete cur;
 	delete res;
 }
 
 static void get2DDist(const ibis::part*& part, const char *col1, double min1, double max1, uint32_t nb1, int iter1,
- 	const char *col2, double min2, double max2, uint32_t nb2, int iter2, const char *qcnd, uint32_t cutoff) {
+ 	const char *col2, double min2, double max2, uint32_t nb2, int iter2, const char *qcnd, uint32_t tally) {
 
-	char combined_cond[200]="";
-	if (qcnd == 0 || *qcnd == 0)
-		sprintf(combined_cond, "1=1");
-	else
-		sprintf(combined_cond, "%s", qcnd);
+	if (debug == 1)
+		printf("get2DDist(part,%s,%f,%f,%d,%d,%s,%f,%f,%d,%d,qcnd,%d)\n",col1,min1,max1,nb1,iter1,col2,min2,max2,nb2,iter2,tally);
 
-//	printf("\n\nget2DDist(%s, %s, %f, %f, %d, %d, %s, %f, %f, %d, %d, %s, %d)\n\n", part->name(), col1, min1, max1, nb1, iter1, col2, min2, max2, nb2, iter2, qcnd, cutoff);
+	if (iter1 == 0 && iter2 == 0) {
+		fake2DDist(col1,min1,max1,col2,min2,max2,qcnd);
+		return;
+	}
 
 	double stride1 = ibis::util::incrDouble((max1-min1)/nb1);
 	double stride2 = ibis::util::incrDouble((max2-min2)/nb2);
    std::vector<uint32_t> cnts;
-	long ierr = part->get2DDistribution(combined_cond,
-		col1, min1, max1, stride1,
-		col2, min2, max2, stride2,
-		cnts);
+
+	long ierr;
+	if (qcnd == 0 || *qcnd == 0) {
+		ierr = part->get2DDistribution("1=1", col1, min1, max1, stride1, col2, min2, max2, stride2, cnts);
+	} else {
+		ierr = part->get2DDistribution(qcnd, col1, min1, max1, stride1, col2, min2, max2, stride2, cnts);
+	}
    if (ierr < 0) {
-		std::cerr << "Warning -- part[" << part->name()
+		std::cerr << "ERROR -- part[" << part->name()
 			       << "].get2DDistribution returned with ierr = " << ierr << std::endl
 					<< "cnts.size() = " << cnts.size() << std::endl;
 		exit(-1);
 	}
-//	printf("min1 %f, max1 %f, stride1 %f\n",min1,max1,stride1);
-//	printf("min2 %f, max2 %f, stride2 %f\n",min2,max2,stride2);
-//	printf("nb1 %d, nb2 %d, cnts.size() %d\n",nb1,nb2,cnts.size());
+
 	// success
+
+	int next_iter1 = (iter1 > 0) ? iter1 - 1 : 0;
+	int next_iter2 = (iter2 > 0) ? iter2 - 1 : 0;
+	uint32_t next_nb1 = (iter1 > 0) ? nb1 : 1;
+	uint32_t next_nb2 = (iter2 > 0) ? nb2 : 1;
 	
+	uint32_t cutoff = pow(nb1,next_iter1) * pow(nb2,next_iter2);
+
+	// count fully loaded bins
+	uint32_t occupied=0;
+	for (uint32_t i = 0; i < cnts.size(); ++ i) {
+		if (cnts[i] > cutoff) {
+			occupied++;
+		}
+	}
+	if (debug == 1)
+		std::cout << "cutoff = " << cutoff << " iter1= " << iter1 << " iter2= " << iter2 << " nb1= " << nb1 << " nb2= " << nb2 << " occupied= " << occupied << " tally= " << tally << std::endl;
+
+	if (occupied >= fudge*nb1*nb2) { // fully loaded and evenly distributed
+		// just output the rectangle
+		if (!first){
+			printf(",");
+		} else {
+			first = 0;
+		}
+		printf("[");
+		printf(f1,min1);printf(",");
+		printf(f1,max1);printf(",");
+		printf(f2,min2);printf(",");
+		printf(f2,max2);printf(",%d,%d]",tally,iter1+iter2);
+		return;
+	}
+
+	// output or refine occupied bins
 	for (uint32_t i = 0; i < cnts.size(); ++ i) {
 		if (cnts[i] > 0) {
-			
-			// i == nb2 * i1 + i2
-			
 			uint32_t i1 = i / nb2;
 			uint32_t i2 = i % nb2;
 			double bmin1 = i1*stride1 + min1;
 			double bmax1 = bmin1 + stride1;
 			double bmin2 = i2*stride2 + min2;
 			double bmax2 = bmin2 + stride2;
-			if (cnts[i] < cutoff && (iter1 > 0 || iter2 > 0)) {
+			if (cnts[i] > 100) {
 				// call it again with modified min1,max1,min2,max2
-				uint32_t next_nb1 = (iter1 > 0) ? nb1 : 1;
-				uint32_t next_nb2 = (iter2 > 0) ? nb2 : 1;
-				int newcutoff = ceil(fudge*cutoff/(next_nb1*next_nb2));
-				get2DDist(part,col1,bmin1,bmax1,next_nb1,iter1-1,
-							   col2,bmin2,bmax2,next_nb2,iter2-1,qcnd,newcutoff);
+				get2DDist(part,col1,bmin1,bmax1,next_nb1,next_iter1,
+							 col2,bmin2,bmax2,next_nb2,next_iter2,qcnd,cnts[i]);
 			} else {
-				if (cnts[i] < cutoff) {
-					// just dump all the points
-					fake2DDist(col1,bmin1,bmax1,col2,bmin2,bmax2,qcnd);
-				} else {
-					if (!first){
-						printf(",");
-					} else {
-						first = 0;
-					}
-					printf("[");//%d,%d,%d,",i,i1,i2);
-					printf(f1,bmin1);printf(",");
-					printf(f1,bmax1);printf(",");
-					printf(f2,bmin2);printf(",");
-					printf(f2,bmax2);
-					printf(",%d,%d]\n",cnts[i],iter1+iter2);
-				}
+				// call direct dumper
+				get2DDist(part,col1,bmin1,bmax1,next_nb1,0,col2,bmin2,bmax2,next_nb2,0,qcnd,cnts[i]);
 			}
 		}
 	}
@@ -388,20 +407,16 @@ int main(int argc, char** argv) {
 	}
 
 	double scale1 = log(nbins1)/log(minbins);
-	int iter1 = 0;
-	if (scale1 > 1) {
+	int iter1 = ceil(scale1);
+	if (iter1 >= 1)
 		nbins1 = minbins;
-		iter1 = ceil(scale1);
-	}
 	double scale2 = log(nbins2)/log(minbins);
-	int iter2 = 0;
-	if (scale2 > 1) {
+	int iter2 = ceil(scale2);
+	if (iter2 >= 1)
 		nbins2 = minbins;
-		iter2 = ceil(scale2);
-	}
-	int cutoff = ceil(fudge*tally/(nbins1*nbins2));
+
 	get2DDist(parts[0],col1,min1,max1,nbins1,iter1,
-		                col2,min2,max2,nbins2,iter2,qcnd,cutoff);
+		                col2,min2,max2,nbins2,iter2,qcnd,tally);
 	printf("]}\n");
 
 	delete tbl;
