027677f49e5a05b06092cba8256f46fc9b28e246
Shiran Pasternak <shiranpasternak@gmail.com>
2012-01-25 10:33:12 -0500

    (node) Adding an Iris Nodejs port.

--- routes/index.js
+++ routes/index.js
@@ -33,25 +33,3 @@ exports.gwas = function(study, width, height, res) {
         + '</div>'
 		);
 };
-
-exports.allpoints = function(study,res) {
-	res.writeHead(200,{"Content-type":"text/html"});
-	res.end('<script type="text/javascript" src="/static/javascript/jquery-1.7.js"></script>'
-		+ '<script type="text/javascript" src="/static/javascript/canvas_scatter.js"></script>'
-		+ '<script type="text/javascript">$(document).ready(manhattan_plot_dots("scatter",'+study+'))</script>'
-		+ '<div style="position: relative;">'
-		+ ' <canvas id="scatter" width="1100" height="500" style="position: absolute; left: 0; top: 0; z-index: 0;"></canvas>'
-		+ ' <canvas id="scatteri" width="1100" height="500" style="position: absolute; left: 0; top: 0; z-index: 1;"></canvas>'
-		+ '</div>');
-};
-
-exports.jquery_fastbit = function(req, res){
-	console.log("going here")
-	res.render('jquery_fastbit',  { layout:false, title: 'Routing fastbit json jQuery' })
-};
-
-exports.kbase_index = function (req,res){
-    console.log("kbase_index")
-    res.render('kbase_index',  { layout:false})
-    
-};
