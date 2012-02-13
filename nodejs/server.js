9f6a3b538f50f2d28db329f8663e085d5772a2a9
Andrew Olson <olson@cshl.edu>
2012-02-13 17:09:47 -0500

    mongodb in, express-resource out

--- nodejs/server.js
+++ nodejs/server.js
@@ -8,12 +8,14 @@ var IRIS_DATA = IRIS_HOME + '/fastbit/data';
  */
 
 var express = require('express'), // web server
-	Resource = require('express-resource'), // for resourceful routing
     routes = require('./routes'), // main page, info page, workspace, etc
     gzip = require('connect-gzip'), // sends gzipped responses 
     exec = require('child_process').exec, // for launching fastbit executables
     spawn = require('child_process').spawn, // same idea
-    app = module.exports = express.createServer(gzip.gzip());
+    app = module.exports = express.createServer(gzip.gzip()),
+		Db = require('mongodb').Db,
+		Connection = require('mongodb').Connection,
+		Server = require('mongodb').Server;
 
 // Configuration
 app.configure(function() {
@@ -27,55 +29,90 @@ app.configure(function() {
 
 app.set('view options', { pretty: true });
 app.configure('development', function() {
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
+  app.use(express.errorHandler({
+	        dumpExceptions: true,
+	        showStack: true
+	    }));
 });
 
 app.configure('production', function() {
-    app.use(express.errorHandler());
+   app.use(express.errorHandler());
 });
 
-// look into using mongoose and/or mongodb for the metadata db
-//    http://mongoosejs.com
 
-// load data resources
-app.resource('datatype', require('./data/type')); // use this to list all data types?
-// replace these with a loop over the data types in the db (never edit this file)
-// app.resource(dtype, require('./data/'+dtype));
-app.resource('genome', require('./data/genome'));
-app.resource('assembly', require('./data/assembly'));
-app.resource('gwas', require('./data/gwas'));
-app.resource('gene2GO', require('./data/gene2GO'));
+// initialize the mongo db connection
+var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
+var db = new Db('iris', new Server(host, port, {}));
 
-// load widget resources
-app.resource('widget', require('./widgets/widget')); // use this to list all widgets
+// route to list all data types
+app.get('/data', function(req,res) {
+	db.collection('data_types',function(err,collection) {
+		collection.find({}).toArray(function(err, dtypes) {
+			res.json(dtypes);
+		});
+	});
+});
+// routes to add/edit data types - deferred admin/power user feature
+
+// route to get a specific data type
+app.get('/data/:dtype', function(req,res) {
+	db.collection('data_types', function(err,collection) {
+		collection.find({_type_id: req.params.dtype}).toArray(function(err, dtypes) {
+			res.json(dtypes);
+		});
+	});
+});
+
+// route to get metadata of a given type
+app.get('/data/:dtype/:meta_id', function(req,res) {
+	db.collection('metadata', function(err,collection) {
+		collection.find({_type_id: req.params.dtype, _meta_id: req.params.meta_id}).toArray(function(err,meta) {
+			res.json(meta);
+		});
+	});
+});
+
+// route to list all widgets
+app.get('/widget', function(req,res) {
+	db.collection('widgets',function(err,collection) {
+		collection.find({}).toArray(function(err, widgets) {
+			res.json(widgets);
+		});
+	});
+});
+// routes to add/edit widgets - deferred admin/power user feature
+// route to get a specific widget
+app.get('/widget/:widget', function(req,res) {
+	db.collection('widgets', function(err, collection) {
+		collection.find({_widget_id: req.params.widget}).toArray(function(err,widget) {
+			res.json(widget);
+		});
+	});
+});
+
+db.open(function(err,p_client) {
+	if (err) {new Error(err);}
+	// routes to initialize widgets
+	db.collection('widgets', function(err, collection) {
+	 	collection.find({}).each(function(err, widget) {
+			if (widget != null) {
+				console.log(widget);
+				widget._routes.each(function(err, route_obj) {
+					app.get('/widget/:widget/'+route_obj.path, route_obj.fn(req,res));
+				});
+			} else {
+				// done or none
+			}
+		});
+	});
+});
+
 
 // Routes for the various views
 app.get('/', routes.index); // welcome page or the main app?
 app.get('/about', routes.about);
 app.get('/contact', routes.contact);
-
-// load routes for the widgets
-// each widget notifies the client (manager.js) of its routes through the resourceful routing above
-// but the server needs to configure them too.
-// Connect to the metadata db, fetch all the widget routes and set them up here...
-
-// One missing link is some client side code that manages the widgets
-// It needs to:
-// 1. fetch the list of widgets (/widget index fn in widgets/widget.js)
-// 2. fetch the various data types (/datatype index fn in data/type.js)
-// 3. load user session info
-// 4. populate the workspace based on session info and/or url - permalink?)
-// 5. manage communication between widgets
-// 6. expose valid relationships between widgets and data
-
-// other useful features (scope creep here)
-// widgets should be able to dump their data - as a URI?
-// metadata browser
-// widget browser
-// user history
 
 app.listen(IRIS_PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
