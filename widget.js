79543f3c3d9134c43d36ea892c938a0fdfd83a79
Shiran Pasternak <shiranpasternak@gmail.com>
2012-02-15 11:50:46 -0600

    Finally got the demo code to work

---
+++ widget.js
@@ -0,0 +1,26 @@
+/*
+ * widget_prototype.js
+ *
+ * For now, just for documentation purposes, function prototypes that should
+ * be implemented by widgets.
+ */
+function Widget(name) {
+    this.name = name;
+    Widget._widgets = {};
+}
+
+Widget.prototype.render = function(canvasId, args) {
+    alert("Dude, this needs to be implemented, and shit.");
+};
+
+Widget.prototype.getJSON = function(path, callback) {
+    $.getJSON(DataServiceAPI + path, callback);
+};
+
+Widget.createWidget = function(name) {
+    return new(Widget._widgets[name]);
+};
+
+Widget.registerWidget = function(name, widgetClass) {
+    Widget._widgets[name] = widgetClass;
+}
