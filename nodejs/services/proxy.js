e24bf6995d9923acc5529c8e85aaf9fc0bf4d1b7
Shiran Pasternak <shiranpasternak@gmail.com>
2012-03-05 16:10:58 -0500

    Node services controlled through centralized configuration
    Includes port, name, file, and configuration. Services being refactored to make use of a base Iris Node file.

---
+++ nodejs/services/proxy.js
@@ -0,0 +1,46 @@
+var iris = require('./iris-base.js');
+var config = iris.loadConfiguration();
+var app = iris.app;
+iris.configureViews(app);
+var routes = iris.routes;
+
+/* Widget configuration
+ *
+ * Associate widget name (key) with renderer (value)
+ * Rendering JavaScript is assumed to be in DOCSROOT/js/widgets
+ *
+ * TODO: Move to MetaContainer?
+ */
+var widgets = {
+    manhattan: 'manhattan.js',
+    pcoords:   'pcoords.js',
+    chord:     'chord.js',
+	data:      'data.js',
+	barChart:  'barChart.js'
+};
+
+// Routes
+app.get('/', routes.index);
+
+app.get('/404', function(req, res) {
+    res.render('error', { title: 'Page not found',
+                          heading: 'Page not found',
+                          message: 'Now go back to where ya came from.'});
+});
+
+app.get('/widget/:widget', function(req, res) {
+    // TODO: Should this be configured at a more stateful level, e.g., session?
+    var layout = req.query["layout"] != null && req.query["layout"] == 'on';
+    var widget_js = widgets[req.params.widget];
+    if (widget_js == null) {
+        res.redirect('/404');
+    } else {
+        routes.widget(req, res, { widget: req.params.widget, js: widget_js, layout: layout });
+    }
+});
+
+app.get('/examples', routes.examples);
+
+app.listen(config.appPort);
+console.log("service-address http://localhost:%d", app.address().port)
+console.log("service-mode %s", app.settings.env);
