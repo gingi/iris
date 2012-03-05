1f74e307f4f5aa392f4ad64a2a810dd1aa0288fb
Shiran Pasternak <shiranpasternak@gmail.com>
2012-03-05 16:10:58 -0500

    Node services controlled through centralized configuration
    Includes port, name, file, and configuration. Services being refactored to make use of a base Iris Node file.

---
+++ nodejs/services/iris-base.js
@@ -0,0 +1,72 @@
+var NODE_HOME = __dirname + '/..';
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require(NODE_HOME + '/routes');
+var gzip = require('connect-gzip');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var app = express.createServer(gzip.gzip());
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+};
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(allowCrossDomain);
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+exports.app = app;
+exports.routes = routes;
+
+exports.configureViews = function(app) {
+    if (!app) {
+        app = this.app;
+    }
+    app.configure(function() {
+        app.set('views', NODE_HOME + '/views');
+        app.set('view engine', 'jade');
+        app.use(express.static(NODE_HOME + '/../root'));
+    });
+    app.set('view options', { pretty: true });        
+};
+
+exports.loadConfiguration = function() {
+    var path = require('path');
+    var confFile = path.normalize(process.argv[2]);
+    if (!confFile.match(/^\//)) {
+        confFile = path.join(global.process.env.PWD, confFile);
+    }
+    if (confFile == null) {
+        console.log("Configuration file required!");
+        console.log("Usage: node " + process.argv[1] + " <config_file.js>");
+        return;
+    }
+    var config = require(confFile).Config;
+    if (global.process.env.NODE_PORT != null) {
+        config.appPort = global.process.env.NODE_PORT;
+    }
+    return config;
+};
