20d4d2fe0b0a3e89a94adcbaa44e17468a2a79d8
Shiran Pasternak <shiranpasternak@gmail.com>
2012-03-05 22:05:10 -0500

    Starting to work generically on example data.

---
+++ conf/examples-config.js
@@ -0,0 +1,10 @@
+var IRIS_HOME = __dirname + '/..';
+var IRIS_BIN  = IRIS_HOME + '/bin';
+var IRIS_DATA = IRIS_HOME + '/examples';
+
+exports.Config = {
+    APP_ADDRESS:     '127.0.0.1',
+    SOCKET_HOST:     '127.0.0.1',
+    BINDIR:          IRIS_BIN,
+    FASTBIT_DATADIR: IRIS_DATA + "/fastbit",
+};
--- nodejs/services/fastbit.js
+++ nodejs/services/fastbit.js
@@ -1,50 +1,12 @@
-/**
- * Module dependencies.
- */
-
-var express = require('express'),
-    exec = require('child_process').exec,
-    spawn = require('child_process').spawn,
-    app = module.exports = express.createServer();
-    
-var confFile = process.argv[2];
-if (confFile == null) {
-    console.log("Configuration file required!");
-    console.log("Usage: node " + process.argv[1] + " <config_file.js>");
-    return;
-}
-var config = require(confFile).Config;
-
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
-
-// Configuration
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(allowCrossDomain);
-    app.use(app.router);
-});
-
-app.configure('development', function() {
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
-});
-app.configure('production', function() {
-    app.use(express.errorHandler());
-});
+var iris   = require('./iris-base.js');
+var config = iris.loadConfiguration();
+var app    = iris.app;
+var routes = iris.routes;
+var exec   = require('child_process').exec;
 
 function run_command(executable, response, args) {
-    var cmd = config.binDir + '/' + executable
-        + ' -d ' + config.dataDir
+    var cmd = config.BIN_DIR + '/' + executable
+        + ' -d ' + config.FASTBIT_DATA_DIR
         + ' ' + args; // FIXME: Major injection security risk    
     console.log(cmd);
     var scatter = exec(cmd, {
@@ -58,20 +20,15 @@ function run_command(executable, response, args) {
 }
 
 app.get('/fbsql', function(req, res) {
-    run_command('fbsql', res, req.params.args);
+    run_command('fbsql', res, req.params.q);
 });
 
-// Generic scatter
 app.get('/scatter', function(req, res) {
-    run_command('scatter', res, req.params.args);
+    run_command('scatter', res, req.params.q);
 });
 
-// Ranges
 app.get('/ranges', function(req, res) {
-    run_command('ranges', res, req.params.args);
+    run_command('ranges', res, req.params.q);
 });
 
-app.listen(config.appPort);
-app.listen(config.appPort);
-console.log("service-address http://localhost:%d", app.address().port)
-console.log("service-mode %s", app.settings.env);
+iris.startServer();
