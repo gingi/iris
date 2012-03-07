73ef7bd913e1828d2b6dac7367a987dfbadd8c09
Jer-Ming Chia <jermth@gmail.com>
2012-03-07 11:03:15 -0500

    Bar chart with slider of adjusting width.

--- root/js/widgets/barchart.js
+++ root/js/widgets/barchart.js
@@ -5,85 +5,127 @@ function BarChart() {
 BarChart.prototype = new Widget();
 
 BarChart.prototype.render = function(divId, args) {
-
+    
     var widget = this;
-    var trait = getParameterByName('trait');
-    var species = getParameterByName('species');
+    var trait = (args.hasOwnProperty('trait')) ? args['trait'] : 'B11';
+    var species = (args.hasOwnProperty('species')) ? args['species'] : 'at';
 
     var path = "/data/phenotypes/" + trait;
-
     // fetch the phenotype data
+
+    widget.divId = divId;
     this.getJSON(path,
     function(phenotypes) {
+        widget.phenotypes=phenotypes;
+        widget.draw_barChart(20);        
+    });
+//    this.divId = divId;
 
-        var maxScore = 0;
-        var data = [];
-        
-        for (var prop in phenotypes) {
-            if (phenotypes.hasOwnProperty(prop))
-            //notsure whatto do with NA, maybe we should ditch them completely.
-            if (phenotypes[prop] != "NA") {
-                var pair = {};
-                pair.label = prop;
-                pair.value = phenotypes[prop];
-                data.push(pair);
-                if (pair.value > maxScore)
-                    maxScore = pair.value;
-            }
-        }
+    d3.select(document.getElementById(divId))
+     .append("input")
+     .attr("type","range")
+     .attr("id","width-range")
+     .attr("name","range")
+     .attr("min",10)
+     .attr("max",25)
+     .attr('style','width: 100px');
 
-        var max_width = document.getElementById(divId).clientWidth - 20;
-        var w = 22;
-        var rowNmbr = Math.floor(data.length * w / max_width);
-        if ((data.length * w) % max_width > 0) {
-            rowNmbr++;
+    document.getElementById("width-range").addEventListener("change", this.redraw_event(), false);
+}
+
+BarChart.prototype.redraw_on_slider = function (){
+    var width = d3.select('#width-range');
+    var v = width.node().value;
+    v= +v;
+    console.log("value:" + v);
+    this.draw_barChart(v);
+}
+
+BarChart.prototype.redraw_event = function() {
+    var widget = this;
+    return function() {
+        widget.redraw_on_slider();
+    };
+}
+
+BarChart.prototype.draw_barChart = function(width){
+    var phenotypes=this.phenotypes;
+    var divId = this.divId;
+    var w = width;
+    var maxScore = 0;
+    var data = [];
+    
+    console.log("Drawing barchart" + phenotypes);
+    
+    for (var prop in phenotypes) {
+        if (phenotypes.hasOwnProperty(prop))
+
+        //notsure whatto do with NA, maybe we should ditch them completely.
+        if (phenotypes[prop] != "NA") {
+            var pair = {};
+            pair.label = prop;
+            pair.value = phenotypes[prop];
+            data.push(pair);
+            if (pair.value > maxScore)
+                maxScore = pair.value;
         }
-        var rowMax = Math.floor(max_width / w);
+    }
+    console.log(divId);
+
+    var max_width = document.getElementById(divId).clientWidth - 20;
+    
+    var rowNmbr = Math.floor(data.length * w / max_width);
+    if ((data.length * w) % max_width > 0) {
+        rowNmbr++;
+    }
+    var rowMax = Math.floor(max_width / w);
 /*
-        console.log("Number of data points: " + data.length);
-        console.log("Widths: max=" + max_width + " barW=" + w);
-        console.log("Nunber of rows: " + rowNmbr);
-        console.log("Nunber of bars/row: " + rowMax);*/
-        // setting each row to be 120px high
-        var h = 120;
-        
-        var x = d3.scale.linear()
-        .domain([0, 1])
-        .range([0, w]);
+    console.log("Number of data points: " + data.length);
+    console.log("Widths: max=" + max_width + " barW=" + w);
+    console.log("Nunber of rows: " + rowNmbr);
+    console.log("Nunber of bars/row: " + rowMax);*/
+    // setting each row to be 120px high
+    var h = 120;
+    
+    var x = d3.scale.linear()
+    .domain([0, 1])
+    .range([0, w]);
 
-        var y = d3.scale.linear()
-        //.domain([0, h)
-        .domain([0, maxScore])
-        .range([0, h-20]);
+    var y = d3.scale.linear()
+    //.domain([0, h)
+    .domain([0, maxScore])
+    .range([0, h-20]);
 
-        // define svg element
-        this.svg = d3.select(document.getElementById(divId))
-        .append("svg")
-        .attr("class", "chart")
-        .attr("width", max_width)
-        .attr("height", h * rowNmbr);
+    // define svg element
+    d3.select("#" + divId + " .chart").remove();
+    this.svg = d3.select(document.getElementById(divId))
+    .append("svg")
+    .attr("class", "chart")
+    .attr("width", max_width)
+    .attr("height", h * rowNmbr);
 
-        // draw the bars
-        this.svg.selectAll("rect")
-        .data(data)
-        .enter().append("rect")
-        .attr("x",
-        function(d, i) {
-            return (i % rowMax) * w;
-        })
-        .attr("y",
-        function(d, i) {
-            return (h - y(d.value) - .5 + (h * (Math.floor(i/rowMax))));
-        })
-        .attr("height",
-        function(d) {
-            return y(d.value);
-        })
-        .attr("width", w)
-        .attr("stroke", "white")
-        .attr("fill", "steelblue");                
+    // draw the bars
+    this.svg.selectAll("rect")
+    .data(data)
+    .enter().append("rect")
+    .attr("x",
+    function(d, i) {
+        return (i % rowMax) * w;
+    })
+    .attr("y",
+    function(d, i) {
+        return (h - y(d.value) - .5 + (h * (Math.floor(i/rowMax))));
+    })
+    .attr("height",
+    function(d) {
+        return y(d.value);
+    })
+    .attr("width", w)
+    .attr("stroke", "white")
+    .attr("fill", "#888888");                
 
-        // add the labels.
+    // add the labels but only if width if greater than 15
+    if (w >=15){
         this.svg.selectAll("text")
         .data(data)
         .enter().append("text")
@@ -103,10 +145,9 @@ BarChart.prototype.render = function(divId, args) {
         .attr("font-family", "\"Helvetica Neue\",Helvetica,Verdana")
         .attr("font-size", "10")
         .attr("fill", "white")
-        .text(function(d, i) { return d.label });
-        
-
-    });
+        .text(function(d, i) { return d.label });        
+    }
+    
 }
-            
+
 Widget.registerWidget('barChart', BarChart);
