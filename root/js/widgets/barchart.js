11e285fe88ceeb8237ad9591ae0806c8d937790c
Jer-Ming Chia <jermth@gmail.com>
2012-03-15 21:08:00 -0400

    Adding Y-axes and lines, Tufte style.

--- root/js/widgets/barchart.js
+++ root/js/widgets/barchart.js
@@ -19,9 +19,7 @@ BarChart.prototype.render = function(divId, args) {
             widget.phenotypes=phenotypes;
             widget.draw_barChart(20);        
         }
-    );
-//    this.divId = divId;
-
+    )
     d3.select(document.getElementById(divId))
      .append("input")
      .attr("type","range")
@@ -38,7 +36,6 @@ BarChart.prototype.redraw_on_slider = function (){
     var width = d3.select('#width-range');
     var v = width.node().value;
     v= +v;
-    console.log("value:" + v);
     this.draw_barChart(v);
 }
 
@@ -55,55 +52,53 @@ BarChart.prototype.draw_barChart = function(width){
     var w = width;
     var maxScore = 0;
     var data = [];
-    
-    console.log("Drawing barchart", phenotypes);
-    
+       
     for (var prop in phenotypes) {
         if (phenotypes.hasOwnProperty(prop))
-
         //notsure whatto do with NA, maybe we should ditch them completely.
         if (phenotypes[prop] != "NA") {
             var pair = {};
             pair.label = prop;
             pair.value = phenotypes[prop];
             data.push(pair);
-            if (pair.value > maxScore)
-                maxScore = pair.value;
+        
+			if (Number(pair.value) > Number(maxScore)){
+            	maxScore = pair.value;
+            }
         }
+		
     }
-    console.log(divId);
-
-    var max_width = document.getElementById(divId).clientWidth - 20;
+    // seting heights and padding
+    var h = 160;
+	var hGraph = 100;
+	var sidePad = 20;
+	var xpad = 20;
+    
+    var max_width = document.getElementById(divId).clientWidth - sidePad - xpad;
     
     var rowNmbr = Math.floor(data.length * w / max_width);
     if ((data.length * w) % max_width > 0) {
         rowNmbr++;
     }
     var rowMax = Math.floor(max_width / w);
-/*
-    console.log("Number of data points: " + data.length);
-    console.log("Widths: max=" + max_width + " barW=" + w);
-    console.log("Nunber of rows: " + rowNmbr);
-    console.log("Nunber of bars/row: " + rowMax);*/
-    // setting each row to be 120px high
-    var h = 120;
-    
-    var x = d3.scale.linear()
-    .domain([0, 1])
-    .range([0, w]);
 
     var y = d3.scale.linear()
-    //.domain([0, h)
-    .domain([0, maxScore])
-    .range([0, h-20]);
-
-    // define svg element
+        .domain([0, maxScore])
+//        .range([0,(hGraph)]);
+	      .range([(hGraph),0]);
+    
+    var yAxis = d3.svg.axis().scale(y).ticks(5).orient("left");
+    
+	// define svg element
     d3.select("#" + divId + " .chart").remove();
-    this.svg = d3.select(document.getElementById(divId))
+    
+	this.svg = d3.select(document.getElementById(divId))
     .append("svg")
     .attr("class", "chart")
-    .attr("width", max_width)
-    .attr("height", h * rowNmbr);
+    .attr("width", document.getElementById(divId).clientWidth - sidePad)
+    .attr("height", h * rowNmbr)
+	.append("g")
+	.attr("transform", "translate(0,20)");
 
     // draw the bars
     this.svg.selectAll("rect")
@@ -111,20 +106,20 @@ BarChart.prototype.draw_barChart = function(width){
     .enter().append("rect")
     .attr("x",
     function(d, i) {
-        return (i % rowMax) * w;
+        return (i % rowMax) * w + xpad ;
     })
     .attr("y",
     function(d, i) {
-        return (h - y(d.value) - .5 + (h * (Math.floor(i/rowMax))));
+        return (y(d.value) + (h * (Math.floor(i/rowMax))));
     })
     .attr("height",
     function(d) {
-        return y(d.value);
+        return hGraph-y(d.value);
     })
     .attr("width", w)
     .attr("stroke", "white")
     .attr("fill", "#888888");                
-
+	
     // add the labels but only if width if greater than 15
     if (w >=15){
         this.svg.selectAll("text")
@@ -132,23 +127,57 @@ BarChart.prototype.draw_barChart = function(width){
         .enter().append("text")
         .attr("x",
         function(d, i) {
-            return (i % rowMax) * w;
+            return (i % rowMax) * w + xpad;
         })
         .attr("y",
         function(d, i) {
-             return (h - .5 + (h * (Math.floor(i/rowMax))));
+            return (h  + (h * (Math.floor(i/rowMax))) - (h-hGraph-5));
         })
-        .attr("text-anchor", "end") // text-align: right
-        .attr("transform", function(d, i) { return "rotate(90 " + (i % rowMax) * w + ","
-            + (h - .5 + (h * (Math.floor(i/rowMax)))) + ")"; })
+        .attr("text-anchor", "start") // text-align: right
+        .attr("transform", function(d, i) { return "rotate(90 " + ((i % rowMax) * w +xpad) + ","
+         + (h  + (h * (Math.floor(i/rowMax))) - (h-hGraph-5)) + ")"; })
         .attr("dx", "-.35em") // padding-right
         .attr("dy", -6) // vertical-align: middle; TODO: Calculate (Half of font size)
         .attr("font-family", "\"Helvetica Neue\",Helvetica,Verdana")
         .attr("font-size", "10")
-        .attr("fill", "white")
+        .attr("fill", "#888888")
         .text(function(d, i) { return d.label });        
     }
-    
+
+	// an axis for each row
+	for (n =1; n<=rowNmbr; n++){
+		
+/*		TODO: figure out the new d3 axis functions
+		this.svg.append("svg:g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + w + "," + (n-1)*h +  ")")
+        .call(yAxis.tickSubdivide(0).tickSize(6));
+*/
+		
+		this.svg.selectAll("lines")
+		.data(y.ticks(5))
+		.enter().append("line")
+		.attr("x1",xpad)
+		.attr("x2",max_width+w)
+		.attr("y1",y)
+		.attr("y2",y)
+		.attr("transform", "translate(0," + (n-1)*h +  ")")
+		.style("stroke","white");
+		
+		this.svg.selectAll("axis labels")
+		.data(y.ticks(5))
+		.enter().append("text")
+		.attr("x",xpad-5)
+		.attr("y",y)
+		.attr("text-anchor", "end")
+        .attr("font-family", "\"Helvetica Neue\",Helvetica,Verdana")
+        .attr("font-size", "8")
+        .attr("fill", "#888888")
+		.attr("transform", "translate(0," + (n-1)*h +  ")")
+		.text(function(d,i){
+			return d;
+		});				
+	}
 }
 
 Widget.registerWidget('barchart', BarChart);
