70b25c029f6b6c52b8eedcd8cc9474d24c9ee8b7
Shiran Pasternak <shiranpasternak@gmail.com>
2012-03-16 15:40:53 -0400

    All widget functionality is restored.
    Also reformatted a lot of the JS code in the process (snake_case -> camelCase).

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -16,38 +16,6 @@ exports.examples = function(req, res) {
 };
 
 exports.workspace = function(req, res, widgetList) {
-    res.render('workspace', { title: "Workspace", layout: "workspace_layout", list: widgetList });
+    res.render('workspace',
+        { title: "Workspace", layout: "workspace_layout", list: widgetList });
 };
-
-
-// exports.pcoords = function(table, res) {
-//     // if (width == null)  
-//     width = 1100;
-//     // if (height == null)
-//     height = 500;
-//     res.writeHead(200,{"Content-type":"text/html"});
-//     res.end('<script type="text/javascript" src="/static/javascript/jquery.js"></script>'
-//         + '<script type="text/javascript" src="/static/javascript/canvas_pcoords.js"></script>'
-//         + '<script type="text/javascript">$(document).ready(pcoords("pcoords","'+table+'"))</script>'
-//         + '<div style="position: relative; min-width:500px">'
-//         + ' <canvas id="pcoords" width="'+width+'" height="'+height+'" style="position: absolute; left: 0; top: 0; z-index: 0;"></canvas>'
-//         + ' <canvas id="pcoordsi" width="'+width+'" height="'+height+'" style="position: absolute; left: 0; top: 0; z-index: 1;"></canvas>'
-//         + '</div>'
-//         );
-// };
-// 
-// exports.gwas = function(study, width, height, res) {
-//     // if (width == null)  
-//     width = 1100;
-//     // if (height == null)
-//     height = 500;
-//     res.writeHead(200,{"Content-type":"text/html"});
-//     res.end('<script type="text/javascript" src="/static/javascript/jquery.js"></script>'
-//         + '<script type="text/javascript" src="/static/javascript/canvas_scatter.js"></script>'
-//         + '<script type="text/javascript">$(document).ready(manhattan_plot("scatter",'+study+'))</script>'
-//         + '<div style="position: relative; min-width:500px">'
-//         + ' <canvas id="scatter" width="'+width+'" height="'+height+'" style="position: absolute; left: 0; top: 0; z-index: 0;"></canvas>'
-//         + ' <canvas id="scatteri" width="'+width+'" height="'+height+'" style="position: absolute; left: 0; top: 0; z-index: 1;"></canvas>'
-//         + '</div>'
-//         );
-// };
