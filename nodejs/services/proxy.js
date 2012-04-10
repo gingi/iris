0352528c55f8c4037ad05a731f9104decbd2e771
Shiran Pasternak <shiranpasternak@gmail.com>
2012-04-10 15:47:38 -0400

    Renderer 'service' provided by the proxy.

--- nodejs/services/proxy.js
+++ nodejs/services/proxy.js
@@ -4,6 +4,13 @@ iris.configureViews(app);
 var routes = iris.routes;
 var url = require('url');
 var httpProxy = require('http-proxy');
+var fs = require('fs');
+var path = require('path');
+var JS_DIR = iris.IRIS_HOME + '/root/js';
+var RENDERER_JS_DIR = JS_DIR + '/renderers';
+var RENDERER_HTTPPATH = '/js/renderers';
+var WIDGET_JS_DIR   = JS_DIR + '/widgets';
+
 
 /* Widget configuration
  *
@@ -54,6 +61,18 @@ function findWidget(key, val) {
 	return null;
 }
 
+function directoryContents(response, dir) {
+    fs.readdir(dir, function (err, files) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(files));
+    });
+}
+
+function fileNotFound(res) {
+    res.writeHead(404);
+    res.end("File not found.");
+}
+
 // Routes
 app.get('/', routes.index);
 
@@ -63,14 +82,58 @@ app.get('/404', function (req, res) {
                           message: 'Now go back to where ya came from.'});
 });
 
+app.get('/widget', function (req, res) {
+    directoryContents(res, WIDGET_JS_DIR);
+});
+
 app.get('/widget/:widget', function (req, res) {
     // TODO: Should this be configured at a more stateful level, e.g., session?
     var layout = req.query["layout"] != null && req.query["layout"] == 'on';
 	var widget = findWidget("id", req.params.widget);
     if (widget == null) {
-        res.redirect('/404');
+        fileNotFound(res);
     } else {
         routes.widget(req, res, { widget: widget.module, js: widget.js, layout: layout });
+    }
+});
+
+app.get('/renderer', function (req, res) {
+    directoryContents(res, RENDERER_JS_DIR);
+});
+
+app.get('/renderer/:renderer', function (req, res) {
+    if (req.params.renderer.match(/[^\s\w\d\.:\/]/)) {
+        res.writeHead(400);
+        res.end("Illegal URL format.");
+        return;
+    }
+    if (req.params.renderer.match(/.js$/)) {
+        var filename = RENDERER_JS_DIR + '/' + req.params.renderer;
+        path.exists(filename, function (exists) {
+            if (!exists) {
+                fileNotFound(res);
+            } else {
+                fs.readFile(filename, function (err, data) {
+                    res.writeHead(200);
+                    res.write(data);
+                    res.end();
+                });
+            }
+        });
+    } else {
+        var basename = 'renderer.' + req.params.renderer + '.js';
+        var filename = RENDERER_JS_DIR + '/' + basename;
+        var httpPath = RENDERER_HTTPPATH + '/' + basename;
+        path.exists(filename, function (exists) {
+            if (!exists) {
+                fileNotFound(res);
+            } else {
+                routes.renderer(req, res, {
+                    js: httpPath, title: "Renderer",
+                    name: req.params.renderer
+                });
+            }
+        });
     }
 });
 
@@ -90,8 +153,8 @@ app.get('/simple', function (req, res) {
 for (var serviceName in iris.endpoints) {
     var endpoint = iris.endpoints[serviceName];
     for (var p = 0; p < endpoint.paths.length; p++) {
-        var path = endpoint.paths[p];
-        app.get(path + "*", function (req, res) {
+        var endpointPath = endpoint.paths[p];
+        app.get(endpointPath + "*", function (req, res) {
             iris.httpGET(res, serviceName, req.url)
         })
     }
