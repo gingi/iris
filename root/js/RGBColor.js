4c8ab1cc0ffc8e8f57f0ac7a1bf711e3a4f4bb94
Jim Thomason <thomason@cshl.edu>
2012-04-12 17:41:28 -0400

    added lighten/darken color methods

--- root/js/RGBColor.js
+++ root/js/RGBColor.js
@@ -11,3 +11,47 @@ RGBColor.prototype.asString = function () {
 RGBColor.prototype.invert = function() {
     return new RGBColor(255 - this.r, 255 - this.g, 255 - this.b);
 }
+
+RGBColor.prototype.darkenBy = function(amount) {
+    var darker = new RGBColor(this.r,this.g,this.b);
+
+    darker.r -= amount;
+    darker.g -= amount;
+    darker.b -= amount;
+
+    if (darker.r < 0) {
+        darker.r = 0;
+    }
+
+    if (darker.g < 0) {
+        darker.g = 0;
+    }
+
+    if (darker.b < 0) {
+        darker.b = 0;
+    }
+
+    return darker;
+}
+
+RGBColor.prototype.lightenBy = function(amount) {
+    var darker = new RGBColor(this.r,this.g,this.b);
+
+    darker.r += amount;
+    darker.g += amount;
+    darker.b += amount;
+
+    if (darker.r > 255) {
+        darker.r = 255;
+    }
+
+    if (darker.g > 255) {
+        darker.g = 255;
+    }
+
+    if (darker.b > 255) {
+        darker.b = 255;
+    }
+
+    return darker;
+}
