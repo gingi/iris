3b8f951c0a63bdbdcac3e27b801b780d3bd3e2b8
Scott Devoid <sdevoid@gmail.com>
2012-04-12 18:33:20 -0400

    Changes to widgets and proxy

--- nodejs/services/proxy.js
+++ nodejs/services/proxy.js
@@ -10,6 +10,7 @@ var JS_DIR = iris.IRIS_HOME + '/root/js';
 var RENDERER_JS_DIR = JS_DIR + '/renderers';
 var RENDERER_HTTPPATH = '/js/renderers';
 var WIDGET_JS_DIR   = JS_DIR + '/widgets';
+var WIDGET_HTTPPATH = '/js/widgets';
 
 var Iris;
 function serverIris() {
@@ -74,15 +75,6 @@ var widgetList = [
     }
 ];
 
-function findWidget(key, val) {
-	for (var i in widgetList) {
-		if (widgetList[i][key] === val) {
-			return widgetList[i];
-		}
-	}
-	return null;
-}
-
 function directoryContents(response, dir) {
     fs.readdir(dir, function (err, files) {
         response.writeHead(200, { 'Content-Type': 'application/json' });
@@ -93,6 +85,10 @@ function directoryContents(response, dir) {
 function fileNotFound(res) {
     res.writeHead(404);
     res.end("File not found.");
+}
+
+function capWord(word) {
+    return word[0].toUpperCase() + word.substr(1);
 }
 
 // Routes
@@ -111,14 +107,25 @@ app.get('/404', function (req, res) {
 });
 
 app.get('/widget', function (request, response) {
-    var i, f, widgets = [];
+    var i, file, name, widgets = [];
     fs.readdir(WIDGET_JS_DIR, function (err, files) {
         response.writeHead(200, { 'Content-Type': 'application/json' });
-        for (i in files) {
-            f = files[i];
-            var widget = require(WIDGET_JS_DIR + '/' + f);
-            widgets[widgets.length] = widget.about;
-            widgets[widgets.length].filename = f;
+        for (i=0; i<files.length; i++) {
+            file = files[i];
+            if(file.match(/^\./)) {
+                // ignore .filenames
+                continue;
+            }
+            var matches = file.match(/^widget\.(.*)\.js/);
+            if(matches) {
+                // set the name to Name
+                name = capWord(matches[1]);
+                widgets.push({
+                    name : name,
+                    filename : file,
+                    example : iris.uri() + '/widget/' + name,
+                });
+            }
         }
         response.write(JSON.stringify(widgets));
         response.end();
@@ -127,8 +134,55 @@ app.get('/widget', function (request, response) {
 
 app.get('/widget/:widget', function (req, res) {
     // TODO: Should this be configured at a more stateful level, e.g., session?
-    var layout = req.query["layout"] != null && req.query["layout"] == 'on';
-	var widget = findWidget("id", req.params.widget);
+    var layout = req.query.layout !== null && req.query.layout == 'on';
+    var widget;
+    if (req.params.widget.match(/[^\s\w\d\.:\/]/)) {
+        res.writeHead(400);
+        res.end("Illegal URL format.");
+        return;
+    }
+    if (req.params.widget.match(/.js$/)) {
+        // Send the file
+        var filename = RENDERER_JS_DIR + '/' + req.params.widget;
+        path.exists(filename, function (exists) {
+            if (!exists) {
+                fileNotFound(res);
+            } else {
+                fs.readFile(filename, function (err, data) {
+                    res.writeHead(200);
+                    res.write(data);
+                    res.end();
+                });
+            }
+        });
+    } else {
+        if (!this.Iris) {
+            serverIris();
+        }
+        var basename = 'widget.' + req.params.widget + '.js';
+        var filename = WIDGET_JS_DIR + '/' + basename;
+        var httpPath = WIDGET_HTTPPATH + '/' + basename;
+        require(filename);
+        var widget = Iris.registrant(req.params.widget);
+        console.log(widget);
+        var name = req.params.widget, requires = [];
+        if( (widget) &&
+            (widget.hasOwnProperty('requires')) ) {
+            requires = widget['requires']
+        }
+        path.exists(filename, function (exists) {
+            if (!exists) {
+                fileNotFound(res);
+            } else {
+                routes.widget(req, res, {
+                    js: httpPath, title: "widget",
+                    name: name,
+                    requires : requires
+                });
+            }
+        });
+    }
+
     if (widget == null) {
         fileNotFound(res);
     } else {
