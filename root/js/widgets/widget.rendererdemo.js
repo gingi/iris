027229b4609eff02eea83447c61e0415bbd818e1
Tobias Paczian <paczian@jermth.local>
2012-04-13 12:10:28 -0400

    stuff

--- root/js/widgets/widget.rendererdemo.js
+++ root/js/widgets/widget.rendererdemo.js
@@ -15,30 +15,30 @@
     }
 
     widget.display = function (div, args) {
-        div.innerHTML = '';
-        var rend_disp = document.createElement('div');
-	rend_disp.setAttribute('id', 'renderer_display');
-	rend_dist.setAttribute('style', "height: 600px;");
-	div.appendChild(rend_disp);
-	Iris.Renderer.Template.render( { target: rend_disp, data: Iris.Renderer.Template.exampleData() } );
+        div.innerHTML = 'Hello World';
+        // var rend_disp = document.createElement('div');
+	// rend_disp.setAttribute('id', 'renderer_display');
+	// rend_dist.setAttribute('style', "height: 600px;");
+	// div.appendChild(rend_disp);
+	// Iris.Renderer.Template.render( { target: rend_disp, data: Iris.Renderer.Template.exampleData() } );
 
-	var select_disp = document.createElement('div');
-	select_dist.setAttribute('style', "height: 150px;");
-	div.appendChild(select_disp);
+	// var select_disp = document.createElement('div');
+	// select_dist.setAttribute('style', "height: 150px;");
+	// div.appendChild(select_disp);
 
-	var lb = document.createElement('select');
-	lb.setAttribute('id', 'renderer_selection');
-	for (i in Iris._FrameBuilder.available_renderers) {
-	    lb.add(new Option(i, i), null);
-	}
-	select_disp.appendChild(lb);
+	// var lb = document.createElement('select');
+	// lb.setAttribute('id', 'renderer_selection');
+	// for (i in Iris._FrameBuilder.available_renderers) {
+	//     lb.add(new Option(i, i), null);
+	// }
+	// select_disp.appendChild(lb);
 
-	var btn = document.createElement('input');
-	btn.setAttribute('type', 'button');
-	btn.onclick = function () {
-	    Iris._FrameBuilder.test_renderer( { renderer: document.getElementById('renderer_selection').options[document.getElementById('renderer_selection').selectedIndex].value, target: document.getElementById('renderer_display') } );
-	};
-	select_disp.appendChild(btn);
+	// var btn = document.createElement('input');
+	// btn.setAttribute('type', 'button');
+	// btn.onclick = function () {
+	//     Iris._FrameBuilder.test_renderer( { renderer: document.getElementById('renderer_selection').options[document.getElementById('renderer_selection').selectedIndex].value, target: document.getElementById('renderer_display') } );
+	// };
+	// select_disp.appendChild(btn);
 
     };
 
