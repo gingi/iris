d71d4f0a68bb75356a9b0393f30a481bb5832aa0
Paul Frybarger <pfrybarger@gmail.com>
2012-04-19 19:21:24 -0500

    Updated network renderer: cleaned up the code, fixed a few bugs, got dragging working, and removed type complexity. Next step is to try it with a kegg network diagram.

--- root/js/renderers/renderer.network.js
+++ root/js/renderers/renderer.network.js
@@ -21,151 +21,176 @@
             dataFormat: "list of string"
         },
 
-        render: render,
+        render: function(options) {
+            var target = options.target;
+            target.innerHTML = "";
+
+            var data = options.data;
+
+            var nodeHash = {
+                ids: {},
+                types: {}
+            };
+
+            // build hashes for node/edge lookup
+            for (var i=0; i<data.nodes.length; i++) {
+                var node = data.nodes[i];
+                nodeHash.ids[node.id] = node;
+                node.edges = [];
+
+                if (typeof(nodeHash.types[node.type]) === 'undefined') {
+                    nodeHash.types[node.type] = [];
+                }
+
+                nodeHash.types[node.type].push(node);
+            }
+
+            for (var i=0; i<data.edges.length; i++) {
+                var edge = data.edges[i];
+                nodeHash.ids[edge.source].edges.push(['s', edge.id]);
+                nodeHash.ids[edge.target].edges.push(['t', edge.id]);
+            }
+
+            var svg = d3.select(target)
+                .append('svg:svg')
+                .attr('width', options.width)
+                .attr('height', options.height);
+
+            // lines go first so they are on bottom of svg (lower z-index in html lingo)
+            svg.selectAll('line')
+                .data(data.edges)
+                .enter().append('svg:line')
+                .attr('id', function(d) {
+                    return 'edge_' + d.id;
+                })
+                .attr('x1', function(d) {
+                    return nodeHash.ids[d.source].x;
+                }).attr('x2', function(d) {
+                    return nodeHash.ids[d.target].x;
+                }).attr('y1', function(d) {
+                    return nodeHash.ids[d.source].y;
+                }).attr('y2', function(d) {
+                    return nodeHash.ids[d.target].y;
+                }).style('stroke', 'black');
+
+            // TODO: make sure there are nodes for the type
+            for (var type in data.types) {
+                var info = data.types[type];
+                var g = svg.selectAll('none')
+                    .data(nodeHash.types[type])
+                    .enter().append('svg:g')
+                    .attr('transform', function(d) {
+                        return "translate(" + d.x + "," + d.y + ")";
+                    })
+                    .call(d3.behavior.drag()
+                          .origin(Object)
+                          .on('drag', drag));
+
+                var shape = g.append('svg:' + info.shape)
+                    .attr('id', function(d) {
+                        return 'node_' + d.id;
+                    });
+
+                for (var attr in info.attrs) {
+                    shape.attr(attr, info.attrs[attr]);
+                }
+
+                // positioning logic here
+                if (info.dx || info.dy) {
+                    shape.attr('transform', function(d) {
+                        var dx = (typeof info.dx === 'number' ? info.dx : 0);
+                        var dy = (typeof info.dy === 'number' ? info.dy : 0);
+                        return "translate(" + dx + "," + dy + ")";
+                    });
+                } else {
+                    // try to position based on shape
+                    if (info.shape === 'rect') {
+                        shape.attr('transform', function(d) {
+                            // first ensure width and height
+                            var dx = info.attrs.width / 2;
+                                var dy = info.attrs.height / 2;
+                            return "translate(-" + dx + ",-" + dy + ")";
+                        });
+                    }
+                }
+
+                var text = g.append('svg:text')
+                    .attr('text-anchor', 'middle')
+                    .attr('pointer-events', 'none')
+                    .attr('fill', '#FFFFFF')
+                    .attr('opacity', '1')
+                    .text(function(d) {
+                        return d.id;
+                    });
+            }
+
+            // prevent text from being selected (probably doesn't work in ie)
+            // and set the color (should do this via css eventually)
+            $('text').css({
+                '-moz-user-select'   : 'none',
+                '-khtml-user-select' : 'none',
+                '-webkit-user-select': 'none',
+                'user-select'        : 'none'
+            });
+
+            var padding = 10;
+            function drag() {
+                d3.select(this)
+                    .attr('transform', function(d) {
+                        d.x = Math.max(padding, Math.min(options.width  - padding, d3.event.x));
+                        d.y = Math.max(padding, Math.min(options.height - padding, d3.event.y));
+
+                        // move the edge
+                        for (var i=0; i<d.edges.length; i++) {
+                            var edge = d.edges[i];
+
+                            var which = (edge[0] === 's' ? '1' : '2');
+                            d3.select($('#edge_' + edge[1])[0])
+                                .attr('x' + which, d.x)
+                                .attr('y' + which, d.y);
+                        }
+
+                        // translate will move the node
+                        return "translate(" + d.x + "," + d.y + ")";
+                    });
+            }
+        },
 
         exampleData: function() {
-            var nodeSize = 30;
-
             return {
                 types: {
-                    compound: {
-                        shape: 'circle',
+                    star: {
+                        shape: 'polygon',
                         attrs: {
-                            cx: function(node) {
-                                return node.x;
-                            },
-                            cy: function(node) {
-                                return node.y;
-                            }
+                            points: "30,3 12,54 57,18 3,18 48,54"
                         },
-                        defaults: {
-                            r: nodeSize / 2
-                        }
+                        dx: -30,
+                        dy: -30
                     },
-                    enzyme: {
+                    rectangle: {
                         shape: 'rect',
                         attrs: {
-                            x: function(node) {
-                                return node.x - nodeSize;
-                            },
-                            y: function(node) {
-                                return node.y - nodeSize / 2;
-                            }
-                        },
-                        defaults: {
-                            width: nodeSize * 2,
-                            height: nodeSize
+                            width: 48,
+                            height: 30
+                        }
+                    },
+                    ellipse: {
+                        shape: 'ellipse',
+                        attrs: {
+                            rx: 25,
+                            ry: 18
                         }
                     }
                 },
 
-                nodes: [{
-                    x: 300,
-                    y: 100,
-                    id: 1,
-                    type: 'compound'
-                }, {
-                    x: 100,
-                    y: 500,
-                    id: 2,
-                    type: 'enzyme'
-                }, {
-                    x: 500,
-                    y: 500,
-                    id: 3,
-                    type: 'compound'
-                }],
+                nodes: [{ x: 300, y: 100, id: 1, type: 'star'      },
+                        { x: 100, y: 500, id: 2, type: 'rectangle' },
+                        { x: 500, y: 500, id: 3, type: 'ellipse'   }],
 
-                edges: [{
-                    source: 1,
-                    target: 2,
-                    id: '1to2'
-                }, {
-                    source: 2,
-                    target: 3,
-                    id: '2to3'
-                }, {
-                    source: 3,
-                    target: 1,
-                    id: '3to1'
-                }]
+                edges: [{ source: 1, target: 2, id: '1to2' },
+                        { source: 2, target: 3, id: '2to3' },
+                        { source: 3, target: 1, id: '3to1' }]
             };
         }
     });
-
-    function render(options) {
-        var target = options.target;
-        target.innerHTML = "";
-
-        var data = options.data;
-
-        var nodeHash = {
-            ids: {},
-                types: {}
-        };
-
-        for (var i = 0; i < data.nodes.length; i++) {
-            var node = data.nodes[i];
-            nodeHash.ids[node.id] = node;
-
-            if (typeof(nodeHash.types[node.type]) === 'undefined') {
-                nodeHash.types[node.type] = [];
-            }
-
-            nodeHash.types[node.type].push(node);
-        }
-
-        var vis = d3.select(target).append('svg').attr('width', options.width).attr('height', options.height);
-
-
-        for (var type in data.types) {
-            var ret = vis.selectAll(data.types[type].shape).data(nodeHash.types[type]).enter().append(data.types[type].shape);
-
-            for (var attr in data.types[type].attrs) {
-                (function(type, attr) {
-                    ret.attr(attr, function(d, i) {
-                        return data.types[type].attrs[attr](d);
-                    });
-                })(type, attr);
-            }
-
-            for (var def in data.types[type].defaults) {
-                var val = data.types[type].defaults[def];
-                ret.attr(def, val);
-            }
-
-            // TODO: figure out weird 2nd drag jump bug, hack now to fix
-            var num = 0;
-            ret.call(d3.behavior.drag()
-                     .on("dragstart", function(d, i) {num = 0;})
-                     .on("drag", function(d, i) {
-                         num++;
-                         if (num === 2) {
-                             return false;
-                         }
-
-                         d.x += d3.event.dx;
-                         d.y += d3.event.dy;
-                         render(settings);
-                     }));
-        }
-
-        vis.selectAll('line').data(data.edges).enter().append('line').attr('x1', function(d, i) {
-            return nodeHash.ids[d.source].x;
-        }).attr('x2', function(d, i) {
-            return nodeHash.ids[d.target].x;
-        }).attr('y1', function(d, i) {
-            return nodeHash.ids[d.source].y;
-        }).attr('y2', function(d, i) {
-            return nodeHash.ids[d.target].y;
-        }).style('stroke', 'black');
-
-        vis.selectAll('text').data(data.nodes).enter().append('text').attr('x', function(d, i) {
-            return d.x;
-        }).attr('y', function(d, i) {
-            return d.y;
-        }).attr('fill', '#FFFFFF').text(function(d, i) {
-            return d.id;
-        });
-    }
-}).call(this);
+})();
