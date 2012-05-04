db19a022aaba1c6dc667785ffe6def689641a451
Andrew Olson <olson@cshl.edu>
2012-05-04 16:57:16 -0400

    ajaxWatcher badges

--- root/js/ajaxWatcher.js
+++ root/js/ajaxWatcher.js
@@ -2,35 +2,53 @@ var delay = 2000;
 var delay2 = 5000;
 var nothingToSee = "none";
 var aWatch;
+var spinner;
 var requestStatus = new Object();
 var requestTimeout = new Object();
+var activeRequests = 0;
+var errors = 0;
 $(function () {
+	spinner = $(".ajaxSpinner");
 	aWatch = $(".ajaxWatch");
 	aWatch.popover({placement:'bottom', content: statusReport });
 	aWatch.ajaxStart(function() {
 		requestStatus = {};
 		requestTimeout = {};
-		aWatch.spin("small");
+		activeRequests = 0;
+		errors = 0;
+		spinner.spin({ lines: 8, length: 2, width: 2, radius: 3, left: -20 });
 	});
 	aWatch.ajaxSend(function(e, jqxhr, settings) {
+		activeRequests++;
+		aWatch.html("running "+activeRequests);
 		requestStatus[settings.url] = "sent";
 		requestTimeout[settings.url] = window.setTimeout(statusReport,delay);
 	});
 	aWatch.ajaxError(function(e, jqxhr, settings) {
+		activeRequests--;
+		errors++;
 		requestStatus[settings.url] = "error";
 		window.clearTimeout(requestTimeout[settings.url]);
 		statusReport();
 	});
 	aWatch.ajaxComplete(function(e, jqxhr, settings) {
+		activeRequests--;
+		aWatch.html("running "+activeRequests);
 		requestStatus[settings.url] = "done";
 		window.clearTimeout(requestTimeout[settings.url]);
 	});
 	aWatch.ajaxStop(function() {
 		statusReport();
-		aWatch.spin(false);
+		spinner.spin(false);
+		if (errors > 0) {
+			aWatch.html("error "+errors);
+		} else {
+			aWatch.html("ready");
+		}
 		var cleanupTimout = window.setTimeout(function() {
 			cleanup();
 			statusReport();
+			aWatch.innerText = activeRequests;
 		},delay2);
 	});
 });
