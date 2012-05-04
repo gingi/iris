46ed110ae02b3e43b3fe621f0a177a9758d7e0e7
Andrew Olson <olson@cshl.edu>
2012-05-04 13:48:39 -0400

    more ajaxWatcher

--- root/js/ajaxWatcher.js
+++ root/js/ajaxWatcher.js
@@ -1,24 +1,53 @@
-var requests = new Object();
+var delay = 2000;
+var aWatch;
+var requestStatus = new Object();
+var requestTimeout = new Object();
 $(function () {
-	$(".ajaxWatch").popover({placement:'bottom', content: requests});
-	$(".ajaxWatch").ajaxStart(function() {
-		console.log("started");
-		$(this).popover('show');
+	aWatch = $(".ajaxWatch");
+	aWatch.popover({placement:'bottom', content: requestStatus});
+	aWatch.ajaxStart(function() {
+		requestStatus = {};
+		requestTimeout = {};
+		aWatch.spin("small");
 	});
-	$(".ajaxWatch").ajaxSend(function(e, jqxhr, settings) {
-		console.log("sent", settings.url);
-		requests[settings.url] = "sent";
-		$(this).data('popover').options.content = requests;
-		$(this).popover('show');
+	aWatch.ajaxSend(function(e, jqxhr, settings) {
+		requestStatus[settings.url] = "sent";
+		requestTimeout[settings.url] = window.setTimeout(statusReport,delay);
+	});
+	$(".ajaxWatch").ajaxError(function(e, jqxhr, settings) {
+		requestStatus[settings.url] = "error";
+		window.clearTimeout(requestTimeout[settings.url]);
+		statusReport();
 	});
 	$(".ajaxWatch").ajaxComplete(function(e, jqxhr, settings) {
-		console.log("completed",settings.url);
-		requests[settings.url] = "done";
-		$(this).data('popover').options.content = requests;
-		$(this).popover('show');
+		requestStatus[settings.url] = "done";
+		window.clearTimeout(requestTimeout[settings.url]);
 	});
 	$(".ajaxWatch").ajaxStop(function() {
-		console.log("stopped",requests);
+		statusReport();
+		aWatch.spin(false);
 		$(this).popover('hide');
 	});
 });
+
+function statusReport() {
+	var div = $(document.createElement('div'));
+	var display = 'hide';
+	for (var req in requestStatus) {
+		var span = $(document.createElement('p'));
+		div.append(span);
+		span.append("<b>" + requestStatus[req] + "</b> " + req);
+		switch (requestStatus[req]) {
+			case "sent":
+				display = 'show';
+				break;
+			case "error":
+				display = 'show';
+				break;
+			case "done":
+			default:
+		}
+	}
+	aWatch.data('popover').options.content = div.html();
+	aWatch.popover(display);
+}
