658a144c63f3a3f9d83081715a54fac248d7ba94
Shiran Pasternak <shiranpasternak@gmail.com>
2012-06-16 11:08:33 -0400

    Reactivated widget/renderer unit tests.
    Need to make a bunch of tests pass.

--- test/client/renderer-test.js
+++ test/client/renderer-test.js
@@ -1,55 +1,51 @@
-var target = __dirname + '/../../root/js/renderers/renderer.js';
-var documentStub;
-var Iris;
+var target = __dirname + '/../../root/js/src/renderer.js';
+var requirejs = require('requirejs');
 
-module.exports = {
-    setUp: function (callback) {
-        var sandbox = require('nodeunit').utils.sandbox;
-        documentStub = require(__dirname + '/stubs.js').documentStub;
-        documentStub.Stub.clearElements();
-        var context = sandbox(target, {
-            console: console,
-            document: documentStub
-        });
-        Iris = context.Iris;
-        callback();
-    },
-    tearDown: function (callback) {
-        // delete Iris.Renderer;
-        delete Iris;
-        callback();
-    },
-    createRenderer: function (test) {
-        var renderer = Iris.Renderer.extend({
-            about: { name: "MyRenderer" },
-            render: function () {}
-        });
-        test.equals(renderer, Iris.Renderer.MyRenderer);
-        test.done();
-    },
-    createRendererWithoutAbout: function (test) {
-        test.doesNotThrow(function () {
-            Iris.Renderer.extend({ render: function () {} });
-        }, null, "Should be able to create a non-registered Renderer");
-        test.done();
-    },
-    dropDownRenderer: function (test) {
-        var renderer = Iris.Renderer.DropDown;
-        var elem;
+requirejs.config({
+	baseUrl: __dirname + '/../../root/js',
+	nodeRequire: require,
+});
 
-        documentStub.Stub.clearElements();
-        elem = renderer.render();
-        test.equal(0, elem.children.length);
-        
-        documentStub.Stub.clearElements();
-        elem = renderer.render([
-            { name: 'a', value: 'x' },
-            { name: 'b', value: 'y' }
-        ]);
-        var children = elem.children;
-        test.equal(2, children.length);
-        test.equal('a', children[0].text); test.equal('x', children[0].value);
-        test.equal('b', children[1].text); test.equal('y', children[1].value);
-        test.done();
-    }
-};
+requirejs([target], function (Renderer) {
+	module.exports = {
+		setUp: function (callback) {
+			callback();
+		},
+		tearDown: function (callback) {
+			callback();
+		},
+		createRenderer: function (test) {
+			var renderer = Renderer.extend({
+				about: { name: "MyRenderer" },
+				render: function () {}
+			});
+			test.equals(renderer, Renderer.MyRenderer);
+			test.done();
+		},
+		createRendererWithoutAbout: function (test) {
+			test.doesNotThrow(function () {
+				Iris.Renderer.extend({ render: function () {} });
+			}, null, "Should be able to create a non-registered Renderer");
+			test.done();
+		},
+		dropDownRenderer: function (test) {
+			var renderer = Renderer.DropDown;
+			var elem;
+	
+			documentStub.Stub.clearElements();
+			elem = renderer.render();
+			test.equal(0, elem.children.length);
+			
+			documentStub.Stub.clearElements();
+			elem = renderer.render([
+				{ name: 'a', value: 'x' },
+				{ name: 'b', value: 'y' }
+			]);
+			var children = elem.children;
+			test.equal(2, children.length);
+			test.equal('a', children[0].text); test.equal('x', children[0].value);
+			test.equal('b', children[1].text); test.equal('y', children[1].value);
+			test.done();
+		}
+	};
+});
