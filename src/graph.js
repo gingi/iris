00467f02c4b29764282cf1e928f311c04254f292
Shiran Pasternak <shiranpasternak@gmail.com>
2012-11-20 17:39:23 -0500

    Changed graph API.

--- src/graph.js
+++ src/graph.js
@@ -1,35 +1,86 @@
-var neighbors = {};
+var util = require('util');
 
-function addNeighbors(n1, n2) {
-	var n1n = neighbors[n1];
-	var n2n = neighbors[n2];
-
-	n1n[n2] = 1;
-	n2n[n1] = 1;
-}
-
-function Node() {
+function Node(graph, id) {
 	var meta = {};
 	var node = {
+        id: id,
 		neighbors: function () {
-			var arr = [];
-			for (var obj in neighbors[this]) {
-				arr.push(obj);
-			}
-			return arr;
+			return graph.neighbors(this);
 		},
-		add: function (neighbor) {
-			addNeighbors(this, neighbor);
+		link: function (neighbor) {
+			graph.link(this, neighbor);
+            return this;
 		},
 		attribute: function (key, val) {
 			meta[key] = val;
+            return this;
 		},
 		meta: function () { return meta; }
 	};
-	neighbors[node] = {};
 	return node;
 }
 
-exports.createNode = function () {
-	return new Node();
+function Graph() {
+    var idSequence = 0;
+    var nodes = {};
+    var edges = {};
+    return {
+        link: function (n1, n2, meta) {
+            var key = [n1.id, n2.id].sort().join(" ");
+            edges[key] = { source: n1, target: n2, meta: meta }
+            return this;
+        },
+        createNode: function (id) {
+            id = (id || idSequence++);
+            var node = new Node(this, id);
+            nodes[node.id] = node;
+            return node;
+        },
+        neighbors: function (node) {
+            var arr = [];
+            for (var key in edges) {
+                var edge = edges[key];
+                if (edge.source.id == node.id)      arr.push(edge.target);
+                else if (edge.target.id == node.id) arr.push(edge.source);
+            }
+            return arr;
+        },
+        nodes: function () {
+            var arr = [];
+            for (var k in nodes) { arr.push(nodes[k]); }
+            return arr;
+        },
+        edges: function () {
+            var arr = [];
+            for (var k in edges) { arr.push(edges[k]); }
+            return arr;
+        },
+        json: function () {
+            var jsonNodes = [];
+            var jsonEdges = [];
+            for (var k in nodes) {
+                var node = nodes[k];
+                var meta = node.meta();
+                meta.id = node.id;
+                jsonNodes.push(meta);
+            }
+            for (var key in edges) {
+                var edge = edges[key];
+                var attributes = {};
+                attributes.source = edge.source.id;
+                attributes.target = edge.target.id;
+                for (var a in edge.meta) {
+                    if (edge.meta.hasOwnProperty(a)) {
+                        attributes[a] = edge.meta[a];
+                    }
+                }
+                jsonEdges.push(attributes);
+            }
+            return { nodes: jsonNodes, edges: jsonEdges };
+        }
+    };
+}
+
+exports.createGraph = function () {
+    return new Graph();
 }
--- test/graph-test.js
+++ test/graph-test.js
@@ -1,27 +1,57 @@
 var graph = require(__dirname + '/../src/graph.js');
 
 exports.neighbors = function (test) {
-	var n1 = graph.createNode();
-	var n2 = graph.createNode();
+	var g = graph.createGraph();
+	var n1 = g.createNode();
+	var n2 = g.createNode();
 	test.equal(0, n1.neighbors().length);
 	test.equal(0, n2.neighbors().length);
-	n1.add(n2);
+	n1.link(n2);
 	test.equal(1, n1.neighbors().length);
 	test.equal(1, n2.neighbors().length);
-	test.equal(n2, n1.neighbors()[0]);
-	test.equal(n1, n2.neighbors()[0]);
+	test.deepEqual(n2, n1.neighbors()[0]);
+	test.deepEqual(n1, n2.neighbors()[0]);
 	
 	// Should replace the neighbor;
-	n2.add(n1);
+	n2.link(n1);
 	test.equal(1, n2.neighbors().length);
-	test.equal(n1, n2.neighbors()[0]);
+	test.deepEqual(n1, n2.neighbors()[0]);
+	test.done();
+}
+
+exports.graph = function (test) {
+	var g = graph.createGraph();
+	var n1 = g.createNode(5);
+	var n2 = g.createNode(3);
+	g.link(n1, n2, { weight: 12 });
+	test.equal(2, g.nodes().length);
+	test.equal(1, g.edges().length);
 	test.done();
 }
 
 exports.metadata = function (test) {
-	var node = graph.createNode();
+	var node = graph.createGraph().createNode();
 	node.attribute('a', 'x');
 	node.attribute('b', 'y');
 	test.deepEqual({ a: 'x', b: 'y' }, node.meta());
+	test.done();
+}
+
+exports.chaining = function (test) {
+	var node = graph.createGraph().createNode().attribute("name", "Some node");
+	test.deepEqual({ 'name' : 'Some node' }, node.meta());
+	test.done();
+}
+
+exports.json = function (test) {
+	var g = graph.createGraph();
+	var n1 = g.createNode(3).attribute("name", "Node 1");
+	var n2 = g.createNode(7).attribute("name", "Node 2");
+	n1.link(n2);
+	var json = {
+		"nodes" : [ { id: 3, name: "Node 1" }, { id: 7, name: "Node 2" } ],
+		"edges" : [ { source: 3, target: 7 }]
+	};
+	test.deepEqual(json, g.json());
 	test.done();
 }
