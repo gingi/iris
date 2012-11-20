bf9bd7204fd4123c7e44fd6c5026fe318a1f01ca
Shiran Pasternak <shiranpasternak@gmail.com>
2012-11-20 17:39:23 -0500

    Changed graph API.

--- test/pajek-test.js
+++ test/pajek-test.js
@@ -2,6 +2,8 @@ var pajek = require(__dirname + '/../src/parsers/pajek');
 var fixture = __dirname + '/fixtures/example.pajek';
 
 exports.testParse = function (test) {
-	pajek.parse(fixture);
+	var network = pajek.parse(fixture);
+	var expectedJSON = require(__dirname + '/fixtures/example.pajek.json');
+	// test.deepEqual(expectedJSON, network.json());
 	test.done();
 }
