edbd6fe0ea9219be275e42aa7ff9ee60b9315b47
Shiran Pasternak <shiranpasternak@gmail.com>
2012-11-21 09:51:44 -0500

    Remove quotation marks from name attribute.

--- test/pajek-test.js
+++ test/pajek-test.js
@@ -1,10 +1,19 @@
 var pajek = require(__dirname + '/../src/parsers/pajek');
-var fixture = __dirname + '/fixtures/example.pajek';
 
-exports.testParse = function (test) {
+exports.realParse = function (test) {
+    var fixture = __dirname + '/fixtures/example.pajek';
 	var expectedJSON = require(__dirname + '/fixtures/example.pajek.json');
 	pajek.parse(fixture, function (network) {
 		test.deepEqual(expectedJSON, network.json());
 		test.done();		
-	})
+	});
+}
+
+exports.simpleParse = function (test) {
+    var fixture = __dirname + '/fixtures/example-small.pajek';
+    var expectedJSON = require(__dirname + '/fixtures/example-small.pajek.json');
+    pajek.parse(fixture, function (network) {
+        test.deepEqual(expectedJSON, network.json());
+        test.done();
+    });
 }
