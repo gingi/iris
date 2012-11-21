a9e60c1b3835e05510b33b48b96285d3d46e69c0
Shiran Pasternak <shiranpasternak@gmail.com>
2012-11-21 09:24:47 -0500

    Now parsing Pajek networks. Need to fix JSON test.

--- test/pajek-test.js
+++ test/pajek-test.js
@@ -2,8 +2,9 @@ var pajek = require(__dirname + '/../src/parsers/pajek');
 var fixture = __dirname + '/fixtures/example.pajek';
 
 exports.testParse = function (test) {
-	var network = pajek.parse(fixture);
 	var expectedJSON = require(__dirname + '/fixtures/example.pajek.json');
-	// test.deepEqual(expectedJSON, network.json());
-	test.done();
+	pajek.parse(fixture, function (network) {
+		test.deepEqual(expectedJSON, network.json());
+		test.done();		
+	})
 }
