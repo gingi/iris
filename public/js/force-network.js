84f824c1cfd44c73c550dbf3bf6c6866303f2769
Shiran Pasternak <shiranpasternak@gmail.com>
2012-12-06 16:20:24 -0500

    Added backbone/underscore. Menus don't work.

--- public/js/force-network.js
+++ public/js/force-network.js
@@ -1,73 +1,82 @@
 requirejs.config({
+    paths: {
+        
+    },
     shim: {
-        jquery:    { exports: '$' },
-        d3:        { exports: 'd3' },
+        jquery:     { exports: '$' },
+        d3:         { exports: 'd3' },
+        backbone:   { exports: 'Backbone' },
+        underscore:  { exports: '_' }
     },
 })
-require(['d3', 'dao', 'jquery'], function (d3, Data, $) {
+require(['d3', 'jquery', 'underscore', 'backbone'], function (d3, $, _, Backbone) {
+    var Network = Backbone.Model.extend({
+       defaults: { name: "" },
+       urlRoot: "/data/network",
+    });
     
-    this.data = new Data();
-    
-    var svg;
-    var width = window.innerWidth,
-        height = window.innerHeight;
+    var networkID = location.hash.substr(1) || 'random';
 
-    var color = d3.scale.category10();
+    var AppView = Backbone.View.extend({
+        initialize: function () {
+            this.width = window.innerWidth;
+            this.height = window.innerHeight;
+            this.color = d3.scale.category10();
+            this.force = d3.layout.force()
+                .charge(-120)
+                .linkDistance(60)
+                .size([this.width, this.height]);
+            var network = this.model;
+            network.set({ id: networkID });
+            network.bind('fetch', this.render);
+            network.fetch({
+                success: function (model) {
+                    App.render();
+                }
+            })
+        },
+        render: function () {
+            var svg = d3.select("body").append("svg");
+            var nodes = this.model.get('nodes');
+            var edges = this.model.get('edges');
+            svg.attr("width", this.width)
+                .attr("height", this.height);
+            this.force.nodes(nodes).links(edges).start();
 
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(60)
-        .size([width, height]);
-
-    var body = d3.select("body");
-
-    // !!! FIXME: User input
-    var networkName = location.hash.substr(1) || 'random';
-
-    data.updateFromServer(networkName, function () { render(); });
-    $(window).on('hashchange', function () {
-        networkName = location.hash.substr(1);
-        data.updateFromServer(networkName, function () { render(); });
-    })
-
-    function render() {
-        body.select("svg").remove();
-        svg = body.append("svg");
+            var link =
+                 svg.selectAll("line.link").data(edges).enter()
+                .append("line")
+                    .attr("class", "link")
+                    .style("stroke-width", function(d) { return d.weight; });
+            var node = svg.selectAll("circle.node").data(nodes).enter()
+                .append("circle")
+                    .attr("class", "node")
+                    .attr("r", 8)
+                    .style("fill", function (d) { return App.color(d.group); })
+                    .on("click", clickNode)
+                    .call(this.force.drag);
         
-        svg.attr("width", width)
-            .attr("height", height);
-        var graph = data.getNetwork(networkName);
-        force
-            .nodes(graph.nodes)
-            .links(graph.edges)
-            .start();
-
-        var link = svg.selectAll("line.link").data(graph.edges).enter()
-            .append("line")
-                .attr("class", "link")
-                .style("stroke-width", function(d) { return d.weight; });
-
-        var node = svg.selectAll("circle.node").data(graph.nodes).enter()
-            .append("circle")
-                .attr("class", "node")
-                .attr("r", 8)
-                .style("fill", function (d) { return color(d.group); })
-                .on("click", clickNode)
-                .call(force.drag);
+            node.append("title").text(function (d) { return d.name; });
+      
+            this.force.on("tick", function() {
+                  link.attr("x1", function(d) { return d.source.x; })
+                      .attr("y1", function(d) { return d.source.y; })
+                      .attr("x2", function(d) { return d.target.x; })
+                      .attr("y2", function(d) { return d.target.y; });
+      
+                  node.attr("cx", function(d) { return d.x; })
+                      .attr("cy", function(d) { return d.y; });
+            });
+            return this;
+        },
+        events: {
+            "click svg": "doClick"
+        },
+        doClick: function () {
+            console.log("Clicked.");
+        }
+    });
         
-        node.append("title").text(function (d) { return d.name; });
-      
-        force.on("tick", function() {
-              link.attr("x1", function(d) { return d.source.x; })
-                  .attr("y1", function(d) { return d.source.y; })
-                  .attr("x2", function(d) { return d.target.x; })
-                  .attr("y2", function(d) { return d.target.y; });
-      
-              node.attr("cx", function(d) { return d.x; })
-                  .attr("cy", function(d) { return d.y; });
-        });
-    }
-    
     var selected, originalFill;
     function clickNode(d) {
         if (selected) {
@@ -92,7 +101,6 @@ require(['d3', 'dao', 'jquery'], function (d3, Data, $) {
         // });
         // $(selected).popover("show");]
         $("#infoBox").empty()
-            // .css('visibility', 'visible')
             .append("<b>Selected:</b> " + $(selected).children('title').text());
         $("#infoBox").fadeIn();
         $("#infoBox").on("click", function () {
@@ -103,4 +111,5 @@ require(['d3', 'dao', 'jquery'], function (d3, Data, $) {
             }
         });
     }
+    var App = new AppView({ model: new Network });
 });
