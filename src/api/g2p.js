8f5d05d6c5fee19f00c41325b9868b5537737ba0
Shiran Pasternak <shiranpasternak@gmail.com>
2012-12-20 23:07:13 -0500

    G2P: Getting genes from selection. Support for lookup. Starting to gain momentum...

--- src/api/g2p.js
+++ src/api/g2p.js
@@ -5,7 +5,7 @@ var sandbox = { jQuery: jQuery, console: console };
 
 requireVanilla(__dirname + '/g2p-api.js');
 module.exports = function (url) {
-    return new sandbox.G2P(url);
+    return new sandbox.Genotype_PhenotypeAPI(url);
 }
 
 function requireVanilla(path) {
