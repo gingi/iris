b497eed76affb56eb7dccabb6f614403b629808c
Shiran Pasternak <shiranpasternak@gmail.com>
2012-12-27 12:49:46 -0500

    Some improvements to heatmap, still in progress. Separate demo app (left HTTP chunking demo around).

--- public/js/renderers/heatmap.js
+++ public/js/renderers/heatmap.js
@@ -1,13 +1,14 @@
 define(['jquery', 'd3', 'util/dragbox'], function ($, d3, DragBox) {
     var MAX_CELLS = 6400; // 80 x 80
+    var BORDER_WIDTH = 0.5;
 	function Heatmap(element, options) {
         var self = this;
         element = $(element);
         options = (options || {});
         
         var matrix = [];
-        var rowIndex = {}; var numRows = -1;
-        var colIndex = {}; var numCols = -1;
+        var rowIndex = {}; var numRows = 0;
+        var colIndex = {}; var numCols = 0;
 
         var cellSize = options.cellSize || 5;
 
@@ -15,22 +16,23 @@ define(['jquery', 'd3', 'util/dragbox'], function ($, d3, DragBox) {
 	    var height = element.height();
         
         self.setData = function (data) {
+            if (data == null) return;
+            if (!data.hasOwnProperty('matrix')) {
+                throw new Error("setData(): Required property 'matrix' not found");
+            }
             matrix = data.matrix;
             if (matrix.length > MAX_CELLS) {
                 throw new Error("Too many cells");
             }
             matrix.forEach(function (e) {
-                if (!rowIndex[e[0]]) {
-                    rowIndex[e[0]] = ++numRows;
-                }
-                if (!colIndex[e[1]]) {
-                    colIndex[e[1]] = ++numCols;
-                }
+                if (rowIndex[e[0]] == null) rowIndex[e[0]] = (++numRows - 1);
+                if (colIndex[e[1]] == null) colIndex[e[1]] = (++numCols - 1);
             });
         };
         self.render = function () {
             element.css("position", "relative");
-            cellSize = Math.max(3, Math.floor(element.width() / numRows));
+            cellSize = Math.max(3,
+                Math.floor((element.width() - BORDER_WIDTH * (numRows-2)) / numRows));
     	    var svg  = d3.select("#" + element.attr('id')).append("svg")
     	        .attr("class", "GnBu")
     	        .attr("width", width)
@@ -43,13 +45,19 @@ define(['jquery', 'd3', 'util/dragbox'], function ($, d3, DragBox) {
 
             for (var i = 0; i < matrix.length; i++) {
                 var cell = matrix[i];
-                var row = Math.floor(i / numRows);
-                var col = i % numRows;
+                var row = rowIndex[cell[0]];
+                var col = colIndex[cell[1]];
                 svg.append("rect")
                     .attr("width", cellSize).attr("height", cellSize)
-                    .attr("x", (cellSize + 0.4) * row)
-                    .attr("y", (cellSize + 0.4) * col)
+                    .attr("x", (cellSize + BORDER_WIDTH) * row)
+                    .attr("y", (cellSize + BORDER_WIDTH) * col)
                     .attr("class", "q" + quantize(cell[2]) + "-9");
+                svg.append("text")
+                    .attr("x", (cellSize + BORDER_WIDTH) * row + 5)
+                    .attr("y", (cellSize + BORDER_WIDTH) * col + 5)
+                    .attr("font-size", "6pt")
+                    .attr("alignment-baseline", "middle")
+                    .text([row, col].join(" "));
             }
         }
         return self;
