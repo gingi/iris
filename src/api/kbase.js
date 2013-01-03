aa4486685946ee9205b6c8b175a19db0cd3b1698
Shiran Pasternak <shiranpasternak@gmail.com>
2013-01-03 13:10:21 -0500

    Added getTraitGenes, fixed error handling.

--- src/api/kbase.js
+++ src/api/kbase.js
@@ -3,6 +3,9 @@ var jQuery = require('jquery');
 var fs     = require('fs');
 var path   = require('path');
 
+var P_DECIMALS = 5;
+var FLANKING_DISTANCE = 1e4;
+
 var apis = {
     network: {
         url: 'http://140.221.92.181:7064/KBaseNetworksRPC/networks',
@@ -36,12 +39,6 @@ var apis = {
     }
 }
 
-var KBaseNetworks  = require('./networks');
-var KBaseGenoPheno = require('./g2p');
-var KBaseCDMI      = require('./cdmi');
-
-var P_DECIMALS = 5;
-
 function errorHandler(response, type, errorMessage) {
     type = (type || "Service Error");
     var getError = errorMessage
@@ -63,25 +60,59 @@ function rpcErrorHandler(response) {
 }
 
 function validateParams(target, reqs) {
+    target = (target || {});
+    reqs   = (reqs || []);
     reqs.push('response');
-    reqs.forEach(function (param) {
-        if (!target.hasOwnProperty(param)) {
-            throw new Error(
-                arguments.callee + ": '" + param + "' is a required parameter");
+    try {
+        var missing = [];
+        for (var i = 0; i < reqs.length; i++) {
+            var param = reqs[i];
+            if (!target.hasOwnProperty(param) || target[param] == null) {
+                missing.push(param);
+            }
         }
+        if (missing.length > 0) {
+            throw "Missing required parameter(s) [" + missing.join(" ") + "]";
+        }
+    } catch (err) {
+        if (target.response) {
+            errorHandler(target.response)(err)
+        }
+    }
+    target.callback = (target.callback || function (json) {
+        target.response.contentType = 'json';
+        target.response.send(json);
     });
+    return target;
 }
 
+function api(key) {
+    var params = apis[key];
+    if (!params.object) {
+        var proto = apiRequire(path.join(__dirname, params.src), params.fn);
+        params.object = new proto(params.url);
+    }
+    return params.object;
+}
+
+var sandboxes = {};
+function apiRequire(path, fn) {
+    if (!sandboxes[path]) {
+        var sandbox = sandboxes[path] = { jQuery: jQuery, console: console };
+        var data = fs.readFileSync(path, 'utf8');
+        var ret = vm.runInNewContext(data, sandbox, path);
+    }
+    return sandboxes[path][fn];
+}
+
+/* ADAPTED METHODS */
+
 exports.getVariations = function (params) {
-    params = (params || {});
-    validateParams(params, ['traitId']);
+    params = validateParams(params, ['traitId']);
     params.contigFetcher =
         (params.contigFetcher || api('cdmi').contigs_to_lengths_async);
     params.variationFetcher =
         (params.variationFetcher || api('g2p').traits_to_variations_async);
-    params.callback   = (params.callback || function (json) {
-        params.response.send(json)
-    });
     params.pcutoff    = (params.pcutoff || 1);
     params.variationFetcher(params.traitId, params.pcutoff,
     function (json) {
@@ -128,21 +159,104 @@ exports.getVariations = function (params) {
     }, rpcErrorHandler(params.response));
 }
 
-function api(key) {
-    var params = apis[key];
-    if (!params.object) {
-        var proto = apiRequire(path.join(__dirname, params.src), params.fn);
-        params.object = new proto(params.url);
-    }
-    return params.object;
+exports.getTraitGenes = function (params) {
+    params = validateParams(params, ['traitId', 'pmin', 'pmax', 'loci']);
+    api('g2p').selected_locations_to_genes_async(
+        params.traitId,
+        params.pmin,
+        params.pmax,
+        params.loci,
+        FLANKING_DISTANCE,
+        params.callback,
+        rpcErrorHandler(params.response)
+    );
 }
 
-var sandboxes = {};
-function apiRequire(path, fn) {
-    if (!sandboxes[path]) {
-        var sandbox = sandboxes[path] = { jQuery: jQuery, console: console };
-        var data = fs.readFileSync(path, 'utf8');
-        var ret = vm.runInNewContext(data, sandbox, path);
+exports.getContigLengths = function (params) {
+    params = validateParams(params, ['genomeId']);
+    api('cdmiEntity').get_relationship_IsComposedOf_async(
+        [params.genomeId], ['id'], [], ['id'], function (data) {
+        var ids = [];
+        data.forEach(function (c) { ids.push(c[2].id); });
+        api('cdmi').contigs_to_lengths_async(ids, function (lengths) {
+            params.callback(lengths);
+        }, rpcErrorHandler(params.response));
+    }, rpcErrorHandler(params.response));
+}
+
+exports.getGenomes = function (params) {
+    params = validateParams(params);
+    api('cdmiEntity').all_entities_Genome_async(
+        0, 100, ['id', 'scientific_name'], function (json) {
+        var genomes = [];
+        for (var id in json) {
+            var genome = json[id];
+            genomes.push([ genome.id, genome.scientific_name ]);
+        }
+        params.callback(genomes);
+    }, rpcErrorHandler(params.response));
+}
+
+exports.getExperiments = function (params) {
+    params = validateParams(params, ['genomeId']);
+    api('g2p').get_experiments_async(params.genomeId, function (json) {
+        params.callback(json);
+    }, rpcErrorHandler(params.response));
+}
+
+exports.getTraits = function (params) {
+    params = validateParams(params, ['experimentId']);
+    api('g2p').get_traits_async(params.experimentId, function (json) {
+        json.forEach(function (trait) {
+            trait[1] = trait[1].replace(/:.*$/, '');
+        });
+        params.callback(json);
+    }, rpcErrorHandler(params.response));
+}
+
+exports.getNeighborNetwork = function (params) {
+    params = validateParams(params, ['nodeId']);
+    api('networks').buildFirstNeighborNetwork_async(
+        [ "kb|netdataset.regprecise.301",
+          "kb|netdataset.modelseed.0",
+          "kb|netdataset.ppi.7" ],
+        params.nodeId,
+        ['GENE_CLUSTER'],
+        function (data) {
+            params.callback(transformNetwork(data));
+        },
+        rpcErrorHandler(params.response)
+    );
+}
+
+/* Utility functions */
+Object.clone = function (obj) {
+    return Object.create(
+        Object.getPrototypeOf(obj), 
+        Object.getOwnPropertyNames(obj).reduce(function(memo, name) {
+            return (memo[name] =
+                Object.getOwnPropertyDescriptor(obj, name)) && memo;
+        }, {})
+    );
+}
+
+function transformNetwork(networkJson) {
+    var json = { nodes: [], edges: [] };
+    var nodeMap = {};
+    for (var i in networkJson.nodes) {
+        var node = Object.clone(networkJson.nodes[i]);
+        nodeMap[node.id] = i;
+        node.kbid = node.id;
+        node.group = node.type;
+        node.id = i;
+        json.nodes.push(node);
     }
-    return sandboxes[path][fn];
+    for (var i in networkJson.edges) {
+        var edge = Object.clone(networkJson.edges[i]);
+        edge.source = parseInt(nodeMap[edge.nodeId1]);
+        edge.target = parseInt(nodeMap[edge.nodeId2]);
+        edge.weight = 1;
+        json.edges.push(edge);
+    }
+    return json;
 }
