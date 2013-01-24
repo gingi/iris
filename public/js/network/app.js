46dfc2698b095596cb76c5ee9e6e9484000298a4
Shiran Pasternak <shiranpasternak@gmail.com>
2013-01-23 22:18:44 -0500

    Pulling in network datasets. Adding links.

--- public/js/network/app.js
+++ public/js/network/app.js
@@ -1,9 +1,16 @@
 require(['jquery', 'backbone', 'underscore', 'renderers/network'],
     function ($, Backbone, _, NetworkVis) {
+        
     var Network = Backbone.Model.extend({
         defaults: { name: "" },
         urlRoot: "/data/network",
     });
+    
+    var Neighborhood = Backbone.Model.extend({
+        url: function () {
+            return "/data/node/" + this.get('id') + '/neighbors';
+        }
+    })
     
     var vis;
     var AppView = Backbone.View.extend({
@@ -11,10 +18,6 @@ require(['jquery', 'backbone', 'underscore', 'renderers/network'],
         initialize: function () {
             _.bindAll(this, 'render');
             this.model.on('change', this.render);
-            $("#nav-search").on('submit', function (evt) {
-                var value = evt.target[0].value;
-                vis.highlight(value);
-            });
         },
         render: function () {
             $("#datavis").empty();
@@ -26,21 +29,95 @@ require(['jquery', 'backbone', 'underscore', 'renderers/network'],
             vis.display();
             return this;
         },
-    });    
+    });
+    
+    var NetworkDatasets = Backbone.Model.extend({
+        url: function () {
+            return '/data/node/' + this.get('id') + '/datasets';
+        },
+        parse: function (data) {
+            this.set('datasets', data);
+        }
+    });
+    
+    var DatasetView = Backbone.View.extend({
+        el: $("#container"),
+        initialize: function () {
+            _.bindAll(this, 'render');
+            this.model.on('change', this.render);
+        },
+        template: _.template("<li><a href=\"<%=link%>\" title=\"<%=desc%>\">" +
+                             "<%=name%></a></li>"),
+        render: function () {
+            var self = this;
+            var hud = $("<div>")
+                .addClass("hud")
+                .css("top", 50)
+                .css("left", 20)
+                .css("width", 400);
+            this.$el.append(hud);
+            var list = $("<ul>");
+            var datasets = this.model.get('datasets');
+            hud.empty();
+            if (datasets == null || datasets.length == 0) {
+                hud.text("No datasets");
+            } else {
+                hud.append(list);
+                datasets.forEach(function (ds) {
+                    list.append(self.template({
+                        name: ds.name, desc: ds.description,
+                        link: "#node/" + self.model.get('id') + 
+                              "/" + ds.id
+                    }));
+                })
+                list.append(self.template({
+                    name: 'All', desc: "Use all networks",
+                    link: "#node/" + self.model.get('id') +
+                        "/" + _.pluck(datasets, 'id').join(',')
+                }))
+            }
+            hud.fadeIn();
+        }
+    })
+    
+    var SearchBox = Backbone.View.extend({
+        el: $("#nav-search"),
+        events: {
+            "submit": "search"
+        },
+        search: function (data) {
+            var query = $("#nav-search [type=text]").val();
+            router.navigate("#node/" + query);
+        }
+    });
 
     var App;
     
     var Router = Backbone.Router.extend({
         routes: {
-            "*actions":        "showNetwork"
+            "node/:id":          "networkDatasets",
+            "node/:id/:dataset": "neighborhood",
+            ":network":          "showNetwork"
         },
         showNetwork: function (networkId) {
+            console.log("Showing network")
             var network = new Network;
             networkId = (networkId || 'random');
             network.set({id: networkId});
             App = new AppView({ model: network });
             network.fetch();
+            var search = new SearchBox();
         },
+        networkDatasets: function (nodeId) {
+            var datasets = new NetworkDatasets({ id: nodeId });
+            var datasetView = new DatasetView({ model: datasets });
+            datasets.fetch();
+        },
+        neighborhood: function (id, dataset) {
+            var network = new Neighborhood({ id: id, dataset: dataset });
+            var app = new AppView({ model: network });
+            network.fetch({ data: { datasets: dataset }});
+        }
     });
     var router = new Router;
     Backbone.history.start();
