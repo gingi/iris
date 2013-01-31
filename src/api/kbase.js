0b5a2bfbc57f1964e9a4a500d6df7cf551d3e8f9
Shiran Pasternak <shiranpasternak@gmail.com>
2013-01-31 09:49:49 -0500

    Marcin's network test instance

--- src/api/kbase.js
+++ src/api/kbase.js
@@ -23,7 +23,7 @@ var FLANKING_DISTANCE = 1e5;
 
 var apis = {
     network: {
-        url: 'http://140.221.84.160:7064/KBaseNetworksRPC/networks',
+        url: 'http://140.221.92.76:7064/KBaseNetworksRPC/networks',
         fn:  "KBaseNetworks"
     },
     g2p: {
