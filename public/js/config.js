bb54c76723f4639b4574dfc7038e37a89d7552a2
Shiran Pasternak <shiranpasternak@gmail.com>
2013-02-08 15:10:03 -0500

    Some cleanup and small fixes.

--- public/js/config.js
+++ public/js/config.js
@@ -1,31 +1,25 @@
 requirejs.config({
     baseUrl: "/js",
     paths: {
-        columnfilter: "DataTables/extras/ColumnFilterWidgets"
+        columnfilter: "DataTables/extras/ColumnFilterWidgets",
+        datatables:   "jquery.dataTables"
     },
     shim: {
-        jquery:      { exports: "$" },
-        d3:          { exports: "d3" },
-        underscore:  { exports: "_" },
-        colorbrewer: { exports: "colorbrewer"},
-        backbone:    {
-            exports: "Backbone",
-            deps: [ "underscore", "jquery" ]
-        },
+        jquery:      { exports: "$"           },
+        d3:          { exports: "d3"          },
+        underscore:  { exports: "_"           },
+        colorbrewer: { exports: "colorbrewer" },
+        backbone:    { exports: "Backbone", deps: [ "underscore", "jquery" ] },
         "backbone.localstorage": {
             exports: "Backbone",
             deps: [ "backbone" ]
         },
-        "jquery.dataTables": {
+        datatables: {
             deps: [ "jquery" ],
-            exports: "$",
-            init: function ($) {
-                console.log("Hello");
-                return $.fn.dataTableExt;
-            }
+            exports: "jQuery.fn.dataTableExt"
         },
         columnfilter: {
-            deps: [ "jquery.dataTables" ]
+            deps: [ "datatables" ]
         },
         "util/spin":  { exports: "Spinner" }
     },
