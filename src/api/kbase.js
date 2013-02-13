25fdd28db28db7ebcbe8c875be6bbd8b543a4cab
Shiran Pasternak <shiranpasternak@gmail.com>
2013-02-13 10:34:37 -0500

    Hotfix: Trap AJAX errors within the API (breaks app).

--- src/api/kbase.js
+++ src/api/kbase.js
@@ -109,6 +109,36 @@ function validateParams(target, reqs) {
     return target;
 }
 
+var jqueryAJAXErrorOverridden = false;
+function trapAJAXErrors() {
+    if (jqueryAJAXErrorOverridden) return;
+    jqueryAJAXErrorOverridden = true;
+    var jQueryAjax = $.ajax;
+    $.extend({
+        ajax: function () {
+            var params = arguments[0];
+            params.error = function (jqXHR, exception) {
+                if (jqXHR.status === 0) {
+                    console.error('Not connect.\n Verify Network.');
+                } else if (jqXHR.status == 404) {
+                    console.error('Requested page not found. [404]');
+                } else if (jqXHR.status == 500) {
+                    console.error('Internal Server Error [500].');
+                } else if (exception === 'parsererror') {
+                    console.error('Requested JSON parse failed.');
+                } else if (exception === 'timeout') {
+                    console.error('Time out error.');
+                } else if (exception === 'abort') {
+                    console.error('Ajax request aborted.');
+                } else {
+                    console.error('Uncaught Error.\n' + jqXHR.responseText);
+                }
+            }
+            return jQueryAjax.apply(null, arguments);
+        }
+    });
+}
+
 function api(key) {
     if (exports.debug) {
         var jQueryAjax = $.ajax;
@@ -122,6 +152,7 @@ function api(key) {
         });
         exports.debug = false; // Just avoid this being overloaded again.
     }
+    trapAJAXErrors();
     var params = apis[key];
     if (!params.object) {
         var proto = KBaseAPI[params.fn];
