f538ceba8dc64fd07caee53ce38490128a532ee0
Shiran Pasternak <shiranpasternak@gmail.com>
2013-02-13 01:17:08 -0500

    Added more to the blurb (developers, links). Added a footer.

--- public/js/g2p/blurb.js
+++ public/js/g2p/blurb.js
@@ -15,7 +15,8 @@ define(["jquery"], function ($) {
     }
     
     loadCss("/css/bootstrap-arrows.css");
-    function Blurb(router) {
+    function Blurb(parent, router) {
+        parent = $(parent);
         var div = $("<div>").addClass("hero-unit").css("min-height", 300)
             .append($("<img>", {
                 src: "/img/g2p-icon.png",
@@ -55,7 +56,73 @@ define(["jquery"], function ($) {
                         router.navigate("#trait/kb|g.3907.trait.0", true)
                     }))
                 )
-        return div;
+        parent.append(div);
+        parent.append($("<div>").addClass("row")
+            .append(section(6, "Developers")
+                .append(developerTable())
+                .append($("<small>").text("This project relies on user input and " +
+                    "testing, so please contact us with any suggestions "+
+                    " or comments.")))
+            .append(section(3, "Related Links")
+                .append(link("/", "<strong>KBase Datavis Library<strong>"))
+                .append(link("https://bitbucket.org/gingi/kbase-datavis", "<strong>Project @ Bitbucket<strong>"))
+                .append(link("http://www.kbase.us", "KBase Homepage"))
+                .append(link("http://www.kbase.us/labs", "KBase Labs"))
+                .append(link("http://www.kbase.us/developer-zone", "KBase Developer Zone"))
+            )
+            .append(section(3, "Documentation")
+                .append($("<p>").text("Coming soon!"))
+            )
+        )
+        
+        function link(url, title) {
+            return $("<a>").attr("href", url).html(title).append("<br/>")
+                .before("<i class=\"icon-external-link\"></i>  ");
+        }
+        
+        function section(size, heading) {
+            return $("<div>").addClass("span"+size)
+                .append($("<h3>").text(heading));
+        }
+            
+        function developerTable() {
+            var developers = [
+                [ "Shiran Pasternak", "Developer", "shiran at cshl.edu" ],
+                [ "Ranjan Priya", "Scientist", "pranjan at utk.edu" ],
+                [ "Sunita Kumari", "Scientist", "kumari at cshl.edu" ]
+            ];
+            for (var i in developers) {
+                developers[i][0] =
+                    "<i class='icon-user'></i> " + developers[i][0]; 
+                developers[i][2] =
+                    "<i class='icon-envelope-alt'></i> " + developers[i][2];
+            }
+            var table = $("<table>").addClass("table contact-table")
+            var thead = $("<thead>");
+            var tbody = $("<tbody>");
+            table.append(thead);
+            table.append(tbody);
+            "Name Role Email".split(" ").forEach(function (h) {
+                thead.append($("<th>").text(h))
+            });
+            developers.forEach(function (d) {
+                var tr = $("<tr>");
+                d.forEach(function (it) { tr.append($("<td>").html(it) )});
+                tbody.append(tr);
+            });
+            return table;
+        }
+                
+
+      //     </div>
+      //     <div class="span3">
+      //       <h4>Documentation</h4>
+      //       Docs link goes *here*
+      //     </div>
+      //   </div>
+      // </div>
+        
+        return this;
     }
     return Blurb;
 });
