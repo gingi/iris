aaca6bada45fa55b175b2721ff6823f660efa825
Shiran Pasternak <shiranpasternak@gmail.com>
2013-02-19 23:52:47 -0500

    Forgot to add these boys.

---
+++ public/js/util/help.js
@@ -0,0 +1,35 @@
+define(["jquery", "underscore", "backbone"], function ($, _, Backbone) {
+    var defaults = {
+        title: "Help",
+        target: "#help-link",
+        width: 300
+    };
+    function Help(options) {
+        options = options ? _.clone(options) : {}
+        _.defaults(options, defaults);
+        var View = Backbone.View.extend({
+            el: $(options.target),
+            initialize: function () {
+                this.render();
+            },
+            render: function () {
+                require(["text!" + options.template], function (html) {
+                    $("#help-link").popover({
+                        title: options.title,
+                        content: html,
+                        placement: "bottom",
+                        html: true,
+                        container: "#datavis",
+                        width: options.width
+                    })
+                })
+            },
+            show: function () {
+                this.popover.popover("show");
+            }
+        });
+        return new View();
+    }
+
+    return Help;
+})
