5dc34c60a8a6455a02d297ab300a1c34cea80f2a
Shiran Pasternak <shiranpasternak@gmail.com>
2013-06-25 11:47:22 -0400

    Whitespace tweaks.

--- services/fastbit.js
+++ services/fastbit.js
@@ -5,73 +5,74 @@ var spawn  = require('child_process').spawn;
 var revalidator = require('revalidator');
 
 var field = {
-	partition: {
-		description: 'path to the fastbit partition',
-		type: 'string',
-		pattern: '^[a-zA-Z0-9_/]+$',
-		required: true
-	},
-	column: {
-		description: 'a column in a fastbit partiton',
-		type: 'string',
-		pattern: '^[a-zA-Z0-9_/]+$',
-	},
-	number: {
-		description: 'a numerical value',
-		type: 'string',
+    partition: {
+        description: 'path to the fastbit partition',
+        type: 'string',
+        pattern: '^[a-zA-Z0-9_/]+$',
+        required: true
+    },
+    column: {
+        description: 'a column in a fastbit partiton',
+        type: 'string',
+        pattern: '^[a-zA-Z0-9_/]+$',
+    },
+    number: {
+        description: 'a numerical value',
+        type: 'string',
         pattern: '^\-?[0-9]+(\.[0-9]+)?$'
-	}
+    }
 };
 
 var schema = {
-	fbsql: {
-		properties: {
-			d: field['partition'],
-			s: {
-				description: 'select clause',
-				type: 'string',
-				required: true
-			},
-			w: {
-				description: 'where clause',
-				type: 'string'
-			},
-			o: {
-				description: 'order by clause',
-				type: 'string'
-			}
-		}
-	},
-	scatter: {
-		properties: {
-			d: field['partition'],
-			c1: field['column'],
-			c2: field['column'],
-			n1: field['number'],
-			n2: field['number'],
-			b1: field['number'],
-			b2: field['number'],
-			x1: field['number'],
-			x2: field['number'],
-			a: {
-				description: 'flag to use adaptive mode',
-				type: 'string',
+    fbsql: {
+        properties: {
+            d: field['partition'],
+            s: {
+                description: 'select clause',
+                type: 'string',
+                required: true
+            },
+            w: {
+                description: 'where clause',
+                type: 'string'
+            },
+            o: {
+                description: 'order by clause',
+                type: 'string'
+            }
+        }
+    },
+    scatter: {
+        properties: {
+            d: field['partition'],
+            c1: field['column'],
+            c2: field['column'],
+            n1: field['number'],
+            n2: field['number'],
+            b1: field['number'],
+            b2: field['number'],
+            x1: field['number'],
+            x2: field['number'],
+            a: {
+                description: 'flag to use adaptive mode',
+                type: 'string',
                 pattern: '^1$'
-			}
-		}
-	},
-	ranges: {
-		d: field['partition']
-	},
-	gwas2go: {
-		s: { description: 'study id', type: 'number', required: true },
-		w: { description: 'where clause', type: 'string' }
-	}
+            }
+        }
+    },
+    ranges: {
+        d: field['partition']
+    },
+    gwas2go: {
+        s: { description: 'study id', type: 'number', required: true },
+        w: { description: 'where clause', type: 'string' }
+    }
 }
 
 function runCommand(executable, request, response, args) {
     var cmd = iris.config.BINDIR + '/' + executable;
-	args['d'] = (args.hasOwnProperty('d')) ?
+    args = (args || {});
+    args['d'] = (Object.prototype.hasOwnProperty.call(args, 'd')) ?
         iris.config.FASTBIT_DATADIR + '/' + args['d'] :
         iris.config.FASTBIT_DATADIR;
     var spawnArgs = [];
@@ -104,18 +105,18 @@ function runCommand(executable, request, response, args) {
 }
 
 app.get('/', function (req, res) {
-	res.json(schema);
+    res.json(schema);
 });
 
 app.get('/:command', function (req, res) {
-	var command = req.params.command;
-	var check = revalidator.validate(req.query, schema[command]);
-	if (check['valid']) {
-		runCommand(command, req, res, req.query);
-	} else {
-		res.json(check['errors']);
-	}
-	
+    var command = req.params.command;
+    var check = revalidator.validate(req.query, schema[command]);
+    if (check['valid']) {
+        runCommand(command, req, res, req.query);
+    } else {
+        res.json(check['errors']);
+    }
+    
 });
 
 iris.startService();
