807c25dec34fef641ce91f066a2d5895f3a0ca32
Shiran Pasternak <shiranpasternak@gmail.com>
2013-06-25 15:10:49 -0400

    Added simple widget to show methylation heatmap.

--- public/js/widgets/RegulatoryNetwork.js
+++ public/js/widgets/RegulatoryNetwork.js
@@ -2,7 +2,7 @@ define(["iris"], function (Iris) {
     var Widget = Iris.Widget.extend({
         about: {
             title: "Regulatory Network",
-            name: "RegulatoryNetwork",
+            name: "myRegulatoryNetwork",
             author: "Andrew Olson"
         },
         display: function (element, args) {
