d377cc625019f3903de564c07138829566620e07
Shiran Pasternak <shiranpasternak@gmail.com>
2013-08-19 09:25:30 -0400

    Fixing broken symlinks, ignores.

--- public/js/util/progress.js
+++ public/js/util/progress.js
@@ -1,4 +1,63 @@
 define(['jquery', 'underscore', 'util/spin'], function ($, _, Spinner) {
+    /**
+     * @alias module:util/progress
+     */
+    function Progress(options) {
+        progressCounter++;
+        options = options ? _.clone(options) : {};
+        _.defaults(options, defaults);
+        _.defaults(options.spinner, spinnerDefaults);
+        options.width = options.initialWidth;
+        var $el = $(options.element);
+        var _id = "progress-container-" + progressCounter;
+        var indicator;
+        /** Show the progress indicator
+         *  @function
+         *  @param {string} message The message to display
+         */
+        this.show = function (message) {
+            var container;
+            if (options.fade) {
+                container = $("<div>")
+                    .attr("id", _id)
+                    .css("z-index", 30)
+                    .css("background-color", "rgba(100%, 100%, 100%, 0.8)")
+                    .css("position", "absolute")
+                    .css("top", 0)
+                    .css("left", 0)
+                    .height($el.height())
+                    .width($el.width());
+                $el.append(container);
+            } else {
+                container = $el;
+            }
+            if (!indicator) {
+                indicator = create(options, _id);
+            }
+            indicator.show(container, message);
+        };
+        /**
+         * Set the current progress
+         * @function
+         * @param {number} percent The ratio of completion
+         */
+        this.progress = function (percent) {
+            if (options.type == Progress.BAR) {
+                if (!indicator) {
+                    indicator = create(options);
+                }
+                indicator.width(percent);
+            }
+        };
+        /**
+         * Hide the progress indicator
+         * @function
+         */
+        this.dismiss = function () {
+            if (indicator) indicator.hide();
+        };
+        return this;
+    }
     var _BAR = "bar", _SPIN = "spin";
     var defaults = {
         fade: true,
@@ -56,49 +115,6 @@ define(['jquery', 'underscore', 'util/spin'], function ($, _, Spinner) {
                 }
             };
         }
-    }
-    function Progress(options) {
-        progressCounter++;
-        options = options ? _.clone(options) : {};
-        _.defaults(options, defaults);
-        _.defaults(options.spinner, spinnerDefaults);
-        options.width = options.initialWidth;
-        var $el = $(options.element);
-        var _id = "progress-container-" + progressCounter;
-        var indicator;
-        this.show = function (message) {
-            var container;
-            if (options.fade) {
-                container = $("<div>")
-                    .attr("id", _id)
-                    .css("z-index", 30)
-                    .css("background-color", "rgba(100%, 100%, 100%, 0.8)")
-                    .css("position", "absolute")
-                    .css("top", 0)
-                    .css("left", 0)
-                    .height($el.height())
-                    .width($el.width());
-                $el.append(container);
-            } else {
-                container = $el;
-            }
-            if (!indicator) {
-                indicator = create(options, _id);
-            }
-            indicator.show(container, message);
-        };
-        this.progress = function (percent) {
-            if (options.type == Progress.BAR) {
-                if (!indicator) {
-                    indicator = create(options);
-                }
-                indicator.width(percent);
-            }
-        };
-        this.dismiss = function () {
-            if (indicator) indicator.hide();
-        };
-        return this;
     }
     Progress.BAR  = _BAR;
     Progress.SPIN = _SPIN;
