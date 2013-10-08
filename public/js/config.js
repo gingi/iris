80a958406642003290b66d8c498c6abae91ff9b4
Shiran Pasternak <shiranpasternak@gmail.com>
2013-10-08 15:38:56 -0400

    Missing dep in config.

--- public/js/config.js
+++ public/js/config.js
@@ -6,23 +6,17 @@ requirejs.config({
         slider:       "bootstrap-slider"
     },
     shim: {
-        jquery:      { exports: ["$", "jQuery"] },
-        d3:          { exports: "d3"          },
-        underscore:  { exports: "_"           },
-        colorbrewer: { exports: "colorbrewer" },
-        backbone:    { exports: "Backbone", deps: [ "underscore", "jquery" ] },
-        "backbone.localstorage": {
-            exports: "Backbone",
-            deps: [ "backbone" ]
-        },
-        "jquery-ui": { deps: [ "jquery" ] },
-        slider:     { deps: [ "jquery" ] },
-        datatables: {
-            deps: [ "jquery" ],
-            exports: "jQuery.fn.dataTableExt"
-        },
-        columnfilter: {
-            deps: [ "datatables" ]
-        }
-    },
+        jquery:       { exports: ["$", "jQuery"] },
+        d3:           { exports: "d3"          },
+        underscore:   { exports: "_"           },
+        colorbrewer:  { exports: "colorbrewer" },
+        backbone:     { exports: "Backbone", deps: ["underscore", "jquery"] },
+        bootstrap:    { deps: ["jquery"] },
+        columnfilter: { deps: ["datatables"] },
+        "backbone.localstorage":
+            { exports: "Backbone", deps: ["backbone"] },
+        "jquery-ui":  { deps: ["jquery"] },
+        slider:       { deps: ["jquery"] },
+        datatables:   { deps: ["jquery"], exports: "jQuery.fn.dataTableExt" }
+    }
 })
