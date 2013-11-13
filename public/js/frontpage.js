904f89f7db086470e61b8675bb185a0b2c54da1c
Shiran Pasternak <shiranpasternak@gmail.com>
2013-11-13 15:00:11 -0500

    Restored pcoords with static example data.

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -8,7 +8,7 @@ function ($, _, Backbone, Viewport) {
         .append(li(a("#renderer/pcoords", "Pcoords")))
         .append(li(a("#widget/bubbles", "Bubble Plot")))
         .append(li(a("#renderer/forceDirectedGraph", "Force-directed Graph")))
-        .append(li(a("#widget/hypomethylation", "Conservation vs. Hypomethylation")));
+        // .append(li(a("#widget/hypomethylation", "Conservation vs. Hypomethylation")));
 
     function pair(k, v) {
         var str =
--- public/js/renderers/pcoords.js
+++ public/js/renderers/pcoords.js
@@ -1,13 +1,10 @@
-define(["iris", "jquery", "underscore"], function (Iris, $, _) {
+define(["iris", "jquery", "underscore", "text!/data/pcoords.json"],
+function (Iris, $, _, ExampleData) {
     var PADDING_TOP = 40;
     var PADDING_BOTTOM = 40;
     var PADDING_SIDES = 20;
     var AXIS_WIDTH = 1;
     var pcoordId = 0;
-
-    function buildSQL() {
-        return "total > 3.0";
-    }
 
     /**
       * @class ParallelCoordinatesPlot
@@ -15,7 +12,7 @@ define(["iris", "jquery", "underscore"], function (Iris, $, _) {
       * 
       * @extends Renderer
       */
-    return Iris.Renderer.extend({
+    var ParallelCoords = Iris.Renderer.extend({
         about: {
             title: "Parallel Coordinate Plot",
             name: "ParallelCoordinatesPlot",
@@ -72,30 +69,26 @@ define(["iris", "jquery", "underscore"], function (Iris, $, _) {
             ctxi.strokeStyle = "black";
             ctxi.fillStyle   = "black";
 
-            // fetch the list of columns in the table
-            // and their min and max values
-            var table =
-                args.hasOwnProperty('table') ? args.table : 'sbi_vs_avec';
-            $.getJSON("/pcoords/" + table + "/ranges", function (json) {
-                // TODO: add event handlers
-                var ysize = ctx.canvas.height - PADDING_TOP - PADDING_BOTTOM;
-                var xsize = (ctx.canvas.width - 2 * PADDING_SIDES -
-                    json.length * AXIS_WIDTH) / (json.length - 1);
+            var ranges = this.data.ranges;
+            var ysize = ctx.canvas.height - PADDING_TOP - PADDING_BOTTOM;
+            var xsize = (ctx.canvas.width - 2 * PADDING_SIDES -
+                ranges.length * AXIS_WIDTH) / (ranges.length - 1);
 
-                var offset = PADDING_SIDES;
-                self.drawAxis(ctxi, json[0], offset, ysize);
-                for (var i = 1; i < json.length; i++) {
-                    offset += AXIS_WIDTH;
-                    self.doPcoords(ctx, table, json[i-1], json[i],
-                        offset, offset + xsize, ysize, "context");
-                    self.doPcoords(ctxf, table, json[i-1], json[i],
-                        offset, offset + xsize, ysize, "focus");
-                    offset += xsize;
-                    self.drawAxis(ctxi, json[i], offset, ysize);
-                }
-            });
+            var offset = PADDING_SIDES;
+            self.drawAxis(ctxi, ranges[0], offset, ysize);
+            for (var i = 1; i < ranges.length; i++) {
+                offset += AXIS_WIDTH;
+                self.doPcoords(ctx, ranges[i-1], ranges[i],
+                    offset, offset + xsize, ysize, "context");
+                self.doPcoords(ctxf, ranges[i-1], ranges[i],
+                    offset, offset + xsize, ysize, "focus");
+                offset += xsize;
+                self.drawAxis(ctxi, ranges[i], offset, ysize);
+            }
         },
-
+        exampleData: function () {
+            return JSON.parse(ExampleData);
+        },
         drawAxis: function (ctx, column, offset, ysize) {
             var center = offset + Math.floor(AXIS_WIDTH / 2);
             var top    = PADDING_TOP - 10;
@@ -129,65 +122,69 @@ define(["iris", "jquery", "underscore"], function (Iris, $, _) {
             ctx.fillText(column[0], center - text.width / 2, bottom + 10);
         },
 
-        doPcoords: function
-            (ctx, table, c1_arr, c2_arr, xmin, xmax, ysize, layer) {
+        doPcoords: function (ctx, c1_arr, c2_arr, xmin, xmax, ysize, layer) {
             var self = this;    
-            var nbins = Math.floor(ysize / 1);
-            var url = "/pcoords/" + table + "/scatter" + "?c1=" + c1_arr[0] + "&c2=" + c2_arr[0] + "&b1=" + nbins + "&b2=" + nbins + "&n1=" + c1_arr[1] + "&n2=" + c2_arr[1] + "&x1=" + c1_arr[2] + "&x2=" + c2_arr[2];
-            if (layer === "focus") {
-                url += '&w=' + buildSQL();
-            }
-            // TODO: Make these colors configurable.
-            $.getJSON(url, function (json) {
-                ctx.fillStyle = "black";
-                if (layer === "focus") {
-                    //yellow-orange
-                    self.maxcolor = [220, 140, 0];
-                    self.mincolor = [255, 180, 0];
-                } else {
-                    // gray
-                    self.maxcolor = [10, 10, 10];
-                    self.mincolor = [100, 100, 100];
-                }
-                var rank = [];
-                rank[0] = 1;
-                for (var i = 1; i < json.data.length; i++) {
-                    if (json.data[i - 1][4] === json.data[i][4]) {
-                        rank[i] = rank[i - 1];
-                    } else {
-                        rank[i] = rank[i - 1] + 1;
-                    }
-                }
-                var maxRank = rank[json.data.length - 1];
-                var f1 = ysize / (c1_arr[2] - c1_arr[1]);
-                var f2 = ysize / (c2_arr[2] - c2_arr[1]);
-                for (var i = 0; i < json.data.length; i++) {
-                    var rect = json.data[i];
-                    if (self.colorByDensity) {
-                        var ratio = rect[4] / json.max;
-                        ratio = rank[i] / maxRank;
-                        var r = self.mincolor[0] +
-                            Math.floor(ratio * (self.maxcolor[0] - self.mincolor[0]));
-                        var g = self.mincolor[1] +
-                            Math.floor(ratio * (self.maxcolor[1] - self.mincolor[1]));
-                        var b = self.mincolor[2] +
-                            Math.floor(ratio * (self.maxcolor[2] - self.mincolor[2]));
-                        ctx.fillStyle =
-                            "rgba(" + r + "," + g + "," + b + "," + "0.1)";
-                    }
-                    ctx.beginPath();
-                    ctx.moveTo(xmin,
-                        PADDING_TOP + ysize - f1 * (rect[0] - c1_arr[1]));
-                    ctx.lineTo(xmin,
-                        PADDING_TOP + ysize - f1 * (rect[1] - c1_arr[1]));
-                    ctx.lineTo(xmax,
-                        PADDING_TOP + ysize - f2 * (rect[3] - c2_arr[1]));
-                    ctx.lineTo(xmax,
-                        PADDING_TOP + ysize - f2 * (rect[2] - c2_arr[1]));
-                    ctx.closePath();
-                    ctx.fill();
-                }
+
+            var group = this.data.scatter[layer];
+            var scatter = _.find(group, function (s) {
+                return (s.dim1 == c1_arr[0] && s.dim2 == c2_arr[0]);
             });
+            
+            if (scatter === undefined) {
+                throw new Error("Could not find data for " +
+                    c1_arr[0] + "-" + c2_arr[0] + " (" + layer + ")");
+            }
+            
+            // TODO: Make these colors configurable.
+            ctx.fillStyle = "black";
+            if (layer === "focus") {
+                //yellow-orange
+                self.maxcolor = [220, 140, 0];
+                self.mincolor = [255, 180, 0];
+            } else {
+                // gray
+                self.maxcolor = [10, 10, 10];
+                self.mincolor = [100, 100, 100];
+            }
+            var rank = [];
+            rank[0] = 1;
+            for (var i = 1; i < scatter.data.length; i++) {
+                if (scatter.data[i - 1][4] === scatter.data[i][4]) {
+                    rank[i] = rank[i - 1];
+                } else {
+                    rank[i] = rank[i - 1] + 1;
+                }
+            }
+            var maxRank = rank[scatter.data.length - 1];
+            var f1 = ysize / (c1_arr[2] - c1_arr[1]);
+            var f2 = ysize / (c2_arr[2] - c2_arr[1]);
+            for (var i = 0; i < scatter.data.length; i++) {
+                var rect = scatter.data[i];
+                if (self.colorByDensity) {
+                    var ratio = rect[4] / scatter.max;
+                    ratio = rank[i] / maxRank;
+                    var r = self.mincolor[0] +
+                        Math.floor(ratio * (self.maxcolor[0] - self.mincolor[0]));
+                    var g = self.mincolor[1] +
+                        Math.floor(ratio * (self.maxcolor[1] - self.mincolor[1]));
+                    var b = self.mincolor[2] +
+                        Math.floor(ratio * (self.maxcolor[2] - self.mincolor[2]));
+                    ctx.fillStyle =
+                        "rgba(" + r + "," + g + "," + b + "," + "0.1)";
+                }
+                ctx.beginPath();
+                ctx.moveTo(xmin,
+                    PADDING_TOP + ysize - f1 * (rect[0] - c1_arr[1]));
+                ctx.lineTo(xmin,
+                    PADDING_TOP + ysize - f1 * (rect[1] - c1_arr[1]));
+                ctx.lineTo(xmax,
+                    PADDING_TOP + ysize - f2 * (rect[3] - c2_arr[1]));
+                ctx.lineTo(xmax,
+                    PADDING_TOP + ysize - f2 * (rect[2] - c2_arr[1]));
+                ctx.closePath();
+                ctx.fill();
+            }
         }
     });
+    return ParallelCoords;
 });
